bda3b67dc5f9481857aa7a14478bab3293bb376f
Wenzel Jakob <wenzel@inf.ethz.ch>
2015-12-07 18:25:38 +0100

    add extra documentation CSS on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -116,25 +116,20 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
     html_context = {
         'css_files': [
-            '.static/theme_overrides.css',
+            '_static/theme_overrides.css'
         ]
     }
-
-#import alabaster
-
-#html_theme_path = [alabaster.get_path()]
-#extensions = ['alabaster']
-#html_theme = 'alabaster'
-#html_sidebars = {
-    #'**': [
-        #'about.html',
-        #'navigation.html',
-        #'relations.html',
-        #'searchbox.html'
-    #]
-#}
+else:
+    html_context = {
+        'css_files': [
+            '//media.readthedocs.org/css/sphinx_rtd_theme.css',            
+            '//media.readthedocs.org/css/readthedocs-doc-embed.css',    
+            '_static/theme_overrides.css'
+        ]
+    }
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
