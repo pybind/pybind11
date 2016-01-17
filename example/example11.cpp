9180519d8cffeb1a42e7d69bfd6a57cef9623cf8
Wenzel Jakob <wenzel@inf.ethz.ch>
2016-01-17 22:36:43 +0100

    added an example on using fancier kinds of default arguments

--- example/example11.cpp
+++ example/example11.cpp
@@ -8,11 +8,26 @@
 */
 
 #include "example.h"
+#include <pybind11/stl.h>
 
 void kw_func(int x, int y) { std::cout << "kw_func(x=" << x << ", y=" << y << ")" << std::endl; }
+
+void kw_func4(const std::vector<int> &entries) {
+    std::cout << "kw_func4: ";
+    for (int i : entries)
+        std::cout << i << " ";
+    std::cout << endl;
+}
 
 void init_ex11(py::module &m) {
     m.def("kw_func", &kw_func, py::arg("x"), py::arg("y"));
     m.def("kw_func2", &kw_func, py::arg("x") = 100, py::arg("y") = 200);
     m.def("kw_func3", [](const char *) { }, py::arg("data") = std::string("Hello world!"));
+
+    /* A fancier default argument */
+    std::vector<int> list;
+    list.push_back(13);
+    list.push_back(17);
+
+    m.def("kw_func4", &kw_func4, py::arg("myList") = list);
 }
--- example/example11.py
+++ example/example11.py
@@ -5,11 +5,12 @@ import pydoc
 
 sys.path.append('.')
 
-from example import kw_func, kw_func2, kw_func3
+from example import kw_func, kw_func2, kw_func3, kw_func4
 
 print(pydoc.render_doc(kw_func, "Help on %s"))
 print(pydoc.render_doc(kw_func2, "Help on %s"))
 print(pydoc.render_doc(kw_func3, "Help on %s"))
+print(pydoc.render_doc(kw_func4, "Help on %s"))
 
 kw_func(5, 10)
 kw_func(5, y=10)
@@ -29,3 +30,6 @@ try:
     kw_func2(x=5, y=10, z=12)
 except Exception as e:
     print("Caught expected exception: " + str(e))
+
+kw_func4()
+kw_func4(myList = [1, 2, 3])
