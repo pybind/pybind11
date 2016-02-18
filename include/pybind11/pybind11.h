33f3430d0c1d6c0ac90aa6f3ac62de108fda3f67
Ben Pritchard <ben@bennyp.org>
2016-02-18 15:25:51 -0500

    Add intel warning push/pop

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -18,6 +18,7 @@
 #  pragma warning(disable: 4100) // warning C4100: Unreferenced formal parameter
 #  pragma warning(disable: 4512) // warning C4512: Assignment operator was implicitly defined as deleted
 #elif defined(__ICC) || defined(__INTEL_COMPILER)
+#  pragma warning(push)
 #  pragma warning(disable:2196)  // warning #2196: routine is both "inline" and "noinline"
 #elif defined(__GNUG__) and !defined(__clang__)
 #  pragma GCC diagnostic push
@@ -1055,6 +1056,8 @@ inline function get_overload(const void *this_ptr, const char *name)  {
 NAMESPACE_END(pybind11)
 
 #if defined(_MSC_VER)
+#  pragma warning(pop)
+#elif defined(__ICC) || defined(__INTEL_COMPILER)
 #  pragma warning(pop)
 #elif defined(__GNUG__) and !defined(__clang__)
 #  pragma GCC diagnostic pop
