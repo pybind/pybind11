1a060fd70df0b9089c34b8f60fba6bb62349b498
Wenzel Jakob <wenzel.jakob@epfl.ch>
2016-04-13 18:46:05 +0200

    function for retrieving the header file path (fixes #158)

--- pybind11/__init__.py
+++ pybind11/__init__.py
@@ -1,1 +1,11 @@
 from ._version import version_info, __version__
+
+
+def get_include():
+    import os
+    try:
+        from pip import locations
+        return os.path.dirname(
+            locations.distutils_scheme('pybind11')['headers'])
+    except ImportError:
+        return 'include'
