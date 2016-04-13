057d05613955f347dacca6105c218d261988beec
Wenzel Jakob <wenzel.jakob@epfl.ch>
2016-04-13 14:42:17 +0200

    fix testcases on Python 3.x

--- example/example16.py
+++ example/example16.py
@@ -7,6 +7,6 @@ from example import return_class_1
 from example import return_class_2
 from example import return_none
 
-print(type(return_class_1()))
-print(type(return_class_2()))
-print(type(return_none()))
+print(type(return_class_1()).__name__)
+print(type(return_class_2()).__name__)
+print(type(return_none()).__name__)
