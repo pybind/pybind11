5a2924275c5abc3db0e841afd60c528d894c7bbd
Wenzel Jakob <wenzel.jakob@epfl.ch>
2016-04-22 10:56:58 +0200

    pybind11.get_include(): added *args and **kwargs pass-through parameters

--- pybind11/__init__.py
+++ pybind11/__init__.py
@@ -1,11 +1,11 @@
 from ._version import version_info, __version__
 
 
-def get_include():
+def get_include(*args, **kwargs):
     import os
     try:
         from pip import locations
         return os.path.dirname(
-            locations.distutils_scheme('pybind11')['headers'])
+            locations.distutils_scheme('pybind11', *args, **kwargs)['headers'])
     except ImportError:
         return 'include'
