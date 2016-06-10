4ff7d9403bad7b2468f5802c02c9464386a5edf0
Klemens Morgenstern <klemens.morgenstern@gmx.net>
2016-06-10 03:40:14 +0200

    Updated FILE* handling in exec.h

--- include/pybind11/exec.h
+++ include/pybind11/exec.h
@@ -3,6 +3,8 @@
 
     Copyright (c) 2016 Klemens D. Morgenstern <klemens.morgenstern@gmx.net>
 
+    This code is based on the boost.python implementation.
+
     All rights reserved. Use of this source code is governed by a
     BSD-style license that can be found in the LICENSE file.
 */
@@ -75,36 +77,46 @@ inline object exec_statement (str string, object global = object(), object local
 }
 
 inline object exec_file(str filename, object global = object(), object local = object()) {
-  // Set suitable default values for global and local dicts.
-  if (!global)
-  {
+    // Set suitable default values for global and local dicts.
+    if (!global)
+    {
     if (PyObject *g = PyEval_GetGlobals())
-      global = object(g, true);
+        global = object(g, true);
     else
-      global = dict();
-  }
-  if (!local) local = global;
-  // should be 'char const *' but older python versions don't use 'const' yet.
+        global = dict();
+    }
+    if (!local) local = global;
+    // should be 'char const *' but older python versions don't use 'const' yet.
 
     auto f = static_cast<std::string>(filename);
 
-  // Let python open the file to avoid potential binary incompatibilities.
+    // Let python open the file to avoid potential binary incompatibilities.
 #if PY_VERSION_HEX >= 0x03040000
-  FILE *fs = _Py_fopen(f.c_str(), "r");
+    constexpr static int close_it = 1;
+    FILE *fs = _Py_fopen(f.c_str(), "r");
 #elif PY_VERSION_HEX >= 0x03000000
-  PyObject *fo = Py_BuildValue("s", f.c_str());
-  FILE *fs = _Py_fopen(fo, "r");
-  Py_DECREF(fo);
+    constexpr static int close_it = 1;
+    PyObject *fo = Py_BuildValue("s", f.c_str());
+    FILE *fs = _Py_fopen(fo, "r");
+    Py_DECREF(fo);
 #else
-  PyObject *pyfile = PyFile_FromString(&f.front(), const_cast<char*>("r"));
-  if (!pyfile) throw std::invalid_argument(std::string(f) + " : no such file");
-  object file(pyfile, false);
-  FILE *fs = PyFile_AsFile(file.ptr());
+    constexpr static int close_it = 0;
+    PyObject *pyfile = PyFile_FromString(&f.front(), const_cast<char*>("r"));
+    if (!pyfile)
+    	throw std::invalid_argument(std::string(f) + " : no such file");
+    object file(pyfile, false);
+    FILE *fs = PyFile_AsFile(file.ptr());
 #endif
-  PyObject* res = PyRun_File(fs,
+    if (fs == nullptr)
+    	throw std::invalid_argument(std::string(f) + " : could not be opened");
+
+    PyObject* res = PyRun_FileEx(fs,
                 f.c_str(),
                 Py_file_input,
-        global.ptr(), local.ptr());
+                global.ptr(), local.ptr(),
+				close_it);
+
+
 
   if (res == nullptr)
         throw error_already_set();
