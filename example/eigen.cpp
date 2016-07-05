b063e64b19ee89cbdafc3d2a82c3324e57e8e83f
Ben North <ben@redfrontdoor.org>
2016-07-05 20:01:11 +0100

    Eigen tests: '2*' functions for col-, row-vectors

--- example/eigen.cpp
+++ example/eigen.cpp
@@ -10,6 +10,12 @@
 #include "example.h"
 #include <pybind11/eigen.h>
 
+Eigen::VectorXf double_col(const Eigen::VectorXf& x)
+{ return 2.0f * x; }
+
+Eigen::RowVectorXf double_row(const Eigen::RowVectorXf& x)
+{ return 2.0f * x; }
+
 void init_eigen(py::module &m) {
     typedef Eigen::Matrix<float, 5, 6, Eigen::RowMajor> FixedMatrixR;
     typedef Eigen::Matrix<float, 5, 6> FixedMatrixC;
@@ -22,6 +28,9 @@ void init_eigen(py::module &m) {
     Eigen::MatrixXf mat(5, 6);
     mat << 0, 3, 0, 0, 0, 11, 22, 0, 0, 0, 17, 11, 7, 5, 0, 1, 0, 11, 0,
         0, 0, 0, 0, 11, 0, 0, 14, 0, 8, 11;
+
+    m.def("double_col", &double_col);
+    m.def("double_row", &double_row);
 
     m.def("fixed_r", [mat]() -> FixedMatrixR { 
         return FixedMatrixR(mat);
