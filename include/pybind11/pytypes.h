bf2510ee8650fb5b340c47c3a46a5ae42b732b27
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-07-18 22:24:53 +0100

    Make buffer_info::as_pybuffer a memoryview ctor

--- include/pybind11/pytypes.h
+++ include/pybind11/pytypes.h
@@ -572,10 +572,29 @@ public:
 
 class memoryview : public object {
 public:
-    memoryview(const buffer_info& info) : memoryview(&info.as_pybuffer()) { }
+    memoryview(const buffer_info& info) {
+        static Py_buffer buf { };
+        // Py_buffer uses signed sizes, strides and shape!..
+        static std::vector<Py_ssize_t> py_strides { };
+        static std::vector<Py_ssize_t> py_shape { };
+        buf.buf = info.ptr;
+        buf.itemsize = (Py_ssize_t) info.itemsize;
+        buf.format = const_cast<char *>(info.format.c_str());
+        buf.ndim = (int) info.ndim;
+        buf.len = (Py_ssize_t) info.size;
+        py_strides.clear();
+        py_shape.clear();
+        for (size_t i = 0; i < info.ndim; ++i) {
+            py_strides.push_back((Py_ssize_t) info.strides[i]);
+            py_shape.push_back((Py_ssize_t) info.shape[i]);
+        }
+        buf.strides = py_strides.data();
+        buf.shape = py_shape.data();
+        buf.suboffsets = nullptr;
+        buf.readonly = false;
+        buf.internal = nullptr;
 
-    memoryview(Py_buffer* view)
-    : object(PyMemoryView_FromBuffer(view), false) {
+        m_ptr = PyMemoryView_FromBuffer(&buf);
         if (!m_ptr)
             pybind11_fail("Unable to create memoryview from buffer descriptor");
     }
