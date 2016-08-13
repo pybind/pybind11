0d7a015fb4addedfdf6e3e6402c0384e3c15f517
Ivan Smirnov <i.s.smirnov@gmail.com>
2016-08-13 12:56:51 +0100

    Update numpy docstring test to the new format

--- example/example-numpy-dtypes.py
+++ example/example-numpy-dtypes.py
@@ -73,7 +73,7 @@ check_eq(arr, [((False, 0, 0.0), (True, 1, 1.5)),
                ((False, 2, 3.0), (True, 3, 4.5))], nested_dtype)
 print_rec_nested(arr)
 
-assert create_rec_nested.__doc__.strip().endswith('numpy.ndarray[dtype=NestedStruct]')
+assert create_rec_nested.__doc__.strip().endswith('numpy.ndarray[NestedStruct]')
 
 arr = create_string_array(True)
 print(arr.dtype)
