dbc4bf68edb28ab3c0faa2d0b9ab709248562c7e
Jason Rhinelander <jason@imaginary.ca>
2016-08-28 14:53:04 -0400

    check-style: also report no space in if(/for(/while(

--- tools/check-style.sh
+++ tools/check-style.sh
@@ -6,14 +6,28 @@
 # Invoke as: tools/check-style.sh
 #
 
-found=0
-for f in `grep $'\t' include/ tests/ docs/*.rst -rl`; do
-    if [ "$found" -eq 0 ]; then
+errors=0
+IFS=$'\n'
+found=
+grep $'\t' include/ tests/ docs/*.rst -rl | while read f; do
+    if [ -z "$found" ]; then
         echo -e '\e[31m\e[01mError: found tabs instead of spaces in the following files:\e[0m'
         found=1
+        errors=1
     fi
 
     echo "    $f"
 done
 
-exit $found
+found=
+grep '\<\(if\|for\|while\)(' include/ tests/* -r --color=always | while read line; do
+    if [ -z "$found" ]; then
+        echo -e '\e[31m\e[01mError: found the following coding style problems:\e[0m'
+        found=1
+        errors=1
+    fi
+
+    echo "    $line"
+done
+
+exit $errors
