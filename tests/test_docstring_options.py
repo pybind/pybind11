a00d54f2273fd75ae6edf9e37dace2a4207a2a5a
Alexander Stukowski <stukowski@mm.tu-darmstadt.de>
2016-11-09 12:21:45 +0100

    Removed trailing whitespaces to comply with coding style rules.

--- tests/test_docstring_options.py
+++ tests/test_docstring_options.py
@@ -1,21 +1,21 @@
 from pybind11_tests import ConstructorStats
 
 def test_docstring_options(capture):
-    from pybind11_tests import (test_function1, test_function2, test_function3, 
+    from pybind11_tests import (test_function1, test_function2, test_function3,
                                 test_function4, test_function5, test_function6,
                                 test_function7, DocstringTestFoo)
 
     # docstrings.disable_signatures()
     assert not test_function1.__doc__
-    
+
     assert test_function2.__doc__ == "A custom docstring"
-    
+
     # docstrings.enable_signatures()
     assert test_function3.__doc__ .startswith("test_function3(a: int, b: int) -> None")
-    
+
     assert test_function4.__doc__ .startswith("test_function4(a: int, b: int) -> None")
     assert test_function4.__doc__ .endswith("A custom docstring\n")
-    
+
     # docstrings.disable_signatures()
     # docstrings.disable_user_defined()
     assert not test_function5.__doc__
