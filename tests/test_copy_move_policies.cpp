84209b12f9c6c69753d7ce05edb20d9f03ee449a
Lori A. Burns <lori.burns@gmail.com>
2016-11-16 17:21:17 -0500

    equals needed for test_copy_move_policies to build icpc 2016.3

--- tests/test_copy_move_policies.cpp
+++ tests/test_copy_move_policies.cpp
@@ -21,7 +21,7 @@ struct lacking_copy_ctor : public empty<lacking_copy_ctor> {
     lacking_copy_ctor(const lacking_copy_ctor& other) = delete;
 };
 
-template <> lacking_copy_ctor empty<lacking_copy_ctor>::instance_ {};
+template <> lacking_copy_ctor empty<lacking_copy_ctor>::instance_ = {};
 
 struct lacking_move_ctor : public empty<lacking_move_ctor> {
     lacking_move_ctor() {}
@@ -29,7 +29,7 @@ struct lacking_move_ctor : public empty<lacking_move_ctor> {
     lacking_move_ctor(lacking_move_ctor&& other) = delete;
 };
 
-template <> lacking_move_ctor empty<lacking_move_ctor>::instance_ {};
+template <> lacking_move_ctor empty<lacking_move_ctor>::instance_ = {};
 
 test_initializer copy_move_policies([](py::module &m) {
     py::class_<lacking_copy_ctor>(m, "lacking_copy_ctor")
