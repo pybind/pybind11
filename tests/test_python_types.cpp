d31547b4d4b9079c380bbb65f7020c840091f502
Phil Austin <paustin@eos.ubc.ca>
2017-06-13 20:46:26 -0700

    add print before creation

--- tests/test_python_types.cpp
+++ tests/test_python_types.cpp
@@ -575,6 +575,7 @@ test_initializer python_types([](py::module &m) {
 
     m.def("return_capsule_with_name_and_destructor_3",
         []() {
+            py::print("creating capsule");
             auto capsule=py::capsule((void *) 1234, "pointer type description",
                 [](PyObject *ptr) {
                  if(ptr){
