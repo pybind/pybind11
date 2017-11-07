c07c7bf2f4cf2fc9fda43d9d79f80a720e98d59f
Jason Rhinelander <jason@imaginary.ca>
2017-11-07 10:01:28 -0400

    Fix variable name shadowing warning under MSVC

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -246,10 +246,10 @@ protected:
                 if (!t)
                     pybind11_fail("Internal error while parsing type signature (1)");
                 if (auto tinfo = detail::get_type_info(*t)) {
-                    handle t((PyObject *) tinfo->type);
+                    handle th((PyObject *) tinfo->type);
                     signature +=
-                        t.attr("__module__").cast<std::string>() + "." +
-                        t.attr("__qualname__").cast<std::string>(); // Python 3.3+, but we backport it to earlier versions
+                        th.attr("__module__").cast<std::string>() + "." +
+                        th.attr("__qualname__").cast<std::string>(); // Python 3.3+, but we backport it to earlier versions
                 } else if (rec->is_new_style_constructor && arg_index == 0) {
                     // A new-style `__init__` takes `self` as `value_and_holder`.
                     // Rewrite it to the proper class type.
