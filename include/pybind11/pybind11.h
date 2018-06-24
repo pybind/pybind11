0ad58d887fe4586c873e6458ac663ee2c0ac60fa
Antony Lee <anntzer.lee@gmail.com>
2018-06-24 15:38:09 +0200

    Silence GCC8's -Wcast-function-type. (#1396)
    * Silence GCC8's -Wcast-function-type.
    
    See https://bugs.python.org/issue33012 and PRs linked therein.

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -314,7 +314,7 @@ protected:
             rec->def = new PyMethodDef();
             std::memset(rec->def, 0, sizeof(PyMethodDef));
             rec->def->ml_name = rec->name;
-            rec->def->ml_meth = reinterpret_cast<PyCFunction>(*dispatcher);
+            rec->def->ml_meth = reinterpret_cast<PyCFunction>(reinterpret_cast<void (*) (void)>(*dispatcher));
             rec->def->ml_flags = METH_VARARGS | METH_KEYWORDS;
 
             capsule rec_capsule(rec, [](void *ptr) {
