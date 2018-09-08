77374a7e5f48b64ee8b51de0a7d3783177a67716
Michael Goulding <michael.goulding@live.com>
2018-09-08 07:25:11 -0700

    VS 15.8.0 Preview 4.0 has a bug with alias templates (#1462)
    * VS 15.8.0 Preview 4.0 has a bug with alias templates

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -477,7 +477,7 @@ template <typename...> struct void_t_impl { using type = void; };
 template <typename... Ts> using void_t = typename void_t_impl<Ts...>::type;
 
 /// Compile-time all/any/none of that check the boolean value of all template types
-#ifdef __cpp_fold_expressions
+#if defined(__cpp_fold_expressions) && !(defined(_MSC_VER) && (_MSC_VER < 1916))
 template <class... Ts> using all_of = bool_constant<(Ts::value && ...)>;
 template <class... Ts> using any_of = bool_constant<(Ts::value || ...)>;
 #elif !defined(_MSC_VER)
