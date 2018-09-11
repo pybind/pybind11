9a19306fbf30642ca331d0ec88e7da54a96860f9
Wenzel Jakob <wenzel.jakob@epfl.ch>
2018-09-11 10:12:10 +0200

    bump version to 2.2.4

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -94,7 +94,7 @@
 
 #define PYBIND11_VERSION_MAJOR 2
 #define PYBIND11_VERSION_MINOR 2
-#define PYBIND11_VERSION_PATCH 3
+#define PYBIND11_VERSION_PATCH 4
 
 /// Include Python header, disable linking to pythonX_d.lib on Windows in debug mode
 #if defined(_MSC_VER)
--- pybind11/_version.py
+++ pybind11/_version.py
@@ -1,2 +1,2 @@
-version_info = (2, 2, 3)
+version_info = (2, 2, 4)
 __version__ = '.'.join(map(str, version_info))
