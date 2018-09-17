6fa8cb5bd361cd5bf7eeb43cdf8efb279a67d40b
Axel Huebl <axel.huebl@plasma.ninja>
2018-09-17 14:13:11 +0200

    Test: Numpy Scalar Creation
    I found that te numpy array tests already contained an empty-shaped
    array test, but none with data in it.
    
    Following PEP 3118, scalars have an empty shape and ndim 0. This
    works already and is now also documented/covered by a test.

--- tests/test_numpy_array.py
+++ tests/test_numpy_array.py
@@ -138,6 +138,11 @@ def test_make_c_f_array():
 def test_make_empty_shaped_array():
     m.make_empty_shaped_array()
 
+    # empty shape means numpy scalar, PEP 3118
+    assert m.scalar_int().ndim == 0
+    assert m.scalar_int().shape == ()
+    assert m.scalar_int() == 42
+
 
 def test_wrap():
     def assert_references(a, b, base=None):
