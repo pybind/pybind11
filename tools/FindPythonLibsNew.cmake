652b6a2280b31135f6eb21d9b89c14e3e45aef34
Ryota Suzuki <suzuryo3893@gmail.com>
2018-10-23 18:27:10 +0900

    Fix unintentional escaping of character on Windows (#1574)

--- tools/FindPythonLibsNew.cmake
+++ tools/FindPythonLibsNew.cmake
@@ -100,6 +100,9 @@ if(NOT _PYTHON_SUCCESS MATCHES 0)
 endif()
 
 # Convert the process output into a list
+if(WIN32)
+    string(REGEX REPLACE "\\\\" "/" _PYTHON_VALUES ${_PYTHON_VALUES})
+endif()
 string(REGEX REPLACE ";" "\\\\;" _PYTHON_VALUES ${_PYTHON_VALUES})
 string(REGEX REPLACE "\n" ";" _PYTHON_VALUES ${_PYTHON_VALUES})
 list(GET _PYTHON_VALUES 0 _PYTHON_VERSION_LIST)
