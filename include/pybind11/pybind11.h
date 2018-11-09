5c11b1c871bddc00378cd008eb91d2315934344e
Wenzel Jakob <wenzel.jakob@epfl.ch>
2018-11-09 10:08:02 +0100

    fix for MSVC (alignof(T) for abstract T is not allowed)

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -1055,7 +1055,7 @@ public:
         record.name = name;
         record.type = &typeid(type);
         record.type_size = sizeof(conditional_t<has_alias, type_alias, type>);
-        record.type_align = alignof(conditional_t<has_alias, type_alias, type>);
+        record.type_align = alignof(conditional_t<has_alias, type_alias, type>&);
         record.holder_size = sizeof(holder_type);
         record.init_instance = init_instance;
         record.dealloc = dealloc;
