1b0c6d8598d19475792457a8a34fd27495d6f196
Yannick Jadoul <yannick.jadoul@belgacom.net>
2018-11-15 20:10:35 +0100

    Fixed wrong selection of return_value_policy::automatic/automatic_reference for pointers in py::cast with universal reference

--- include/pybind11/cast.h
+++ include/pybind11/cast.h
@@ -1669,11 +1669,12 @@ T cast(const handle &handle) { return T(reinterpret_borrow<object>(handle)); }
 template <typename T, detail::enable_if_t<!detail::is_pyobject<T>::value, int> = 0>
 object cast(T &&value, return_value_policy policy = return_value_policy::automatic_reference,
             handle parent = handle()) {
+    using no_ref_T = typename std::remove_reference<T>::type;
     if (policy == return_value_policy::automatic)
-        policy = std::is_pointer<T>::value ? return_value_policy::take_ownership :
+        policy = std::is_pointer<no_ref_T>::value ? return_value_policy::take_ownership :
                  std::is_lvalue_reference<T>::value ? return_value_policy::copy : return_value_policy::move;
     else if (policy == return_value_policy::automatic_reference)
-        policy = std::is_pointer<T>::value ? return_value_policy::reference :
+        policy = std::is_pointer<no_ref_T>::value ? return_value_policy::reference :
                  std::is_lvalue_reference<T>::value ? return_value_policy::copy : return_value_policy::move;
     return reinterpret_steal<object>(detail::make_caster<T>::cast(std::forward<T>(value), policy, parent));
 }
