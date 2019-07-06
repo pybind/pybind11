8b90b1da625f7e96ce9164db55b97a48e039b730
Wenzel Jakob <wenzel.jakob@epfl.ch>
2019-07-06 14:52:32 +0200

    error_already_set: acquire GIL one line earlier (fixes #1779)

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -1998,8 +1998,8 @@ class gil_scoped_release { };
 
 error_already_set::~error_already_set() {
     if (m_type) {
-        error_scope scope;
         gil_scoped_acquire gil;
+        error_scope scope;
         m_type.release().dec_ref();
         m_value.release().dec_ref();
         m_trace.release().dec_ref();
