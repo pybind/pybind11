879f4e93e68fddbccfd707061b14b3169505f725
Isuru Fernando <isuruf@gmail.com>
2019-11-19 17:34:16 -0600

    Update get_include

--- pybind11/__init__.py
+++ pybind11/__init__.py
@@ -4,4 +4,9 @@ from ._version import version_info, __version__  # noqa: F401 imported but unuse
 def get_include(user=False):
     import os
     d = os.path.dirname(__file__)
-    return os.path.join(os.path.dirname(d), "include")
+    if os.path.exists(os.path.join(d, "include")):
+        # Package is installed
+        return os.path.join(d, "include")
+    else:
+        # Package is from a source directory
+        return os.path.join(os.path.dirname(d), "include")
