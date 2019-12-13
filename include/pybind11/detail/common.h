c5ce1cf01213a1c1d3f6c9fd2a54285eae915a87
Wenzel Jakob <wenzel.jakob@epfl.ch>
2019-12-13 11:11:33 +0100

    undo #define copysign in pyconfig.h

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -113,6 +113,9 @@
 #include <frameobject.h>
 #include <pythread.h>
 
+/* Python #defines overrides on all sorts of core functions, which
+   tends to weak havok in C++ codebases that expect these to work
+   like regular functions (potentially with several overloads) */
 #if defined(isalnum)
 #  undef isalnum
 #  undef isalpha
@@ -121,6 +124,10 @@
 #  undef isupper
 #  undef tolower
 #  undef toupper
+#endif
+
+#if defined(copysign)
+#  undef copysign
 #endif
 
 #if defined(_MSC_VER)
