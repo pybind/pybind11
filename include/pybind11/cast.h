7254b3b27c728722259c91f098c2e4e7d7796c76
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2019-12-31 00:02:41 +0100

    remove unused cast operators

--- include/pybind11/cast.h
+++ include/pybind11/cast.h
@@ -1587,9 +1587,6 @@ public:
 
     template <typename T> using cast_op_type = detail::movable_cast_op_type<T>;
 
-    explicit operator type*() { return this->value; }
-    explicit operator type&() { return *(this->value); }
-
     // Workaround for Intel compiler bug
     // see pybind11 issue 94
     #if !defined(__ICC) && !defined(__INTEL_COMPILER)
