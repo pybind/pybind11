b1dbc30f0d4dec857d3b8abe1c619644012da1be
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-08-07 15:10:29 +0300

    fix: typo

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -1737,7 +1737,7 @@ template <return_value_policy Policy = return_value_policy::reference_internal,
           typename ValueType = decltype(*std::declval<Iterator>()),
           typename... Extra>
 detail::iterator_state<Iterator, Sentinel, false, Policy>
-[[deprecated]] make_iterator_ng(Iterator first, Sentinel last, Extra &&... extra) {
+make_iterator_ng(Iterator first, Sentinel last, Extra &&... extra) {
     typedef detail::iterator_state<Iterator, Sentinel, false, Policy> state;
 
     if (!detail::get_type_info(typeid(state), false)) {
