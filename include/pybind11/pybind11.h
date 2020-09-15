16f199f8d969c3c01ae0f8c863498bf5854389ea
Yannick Jadoul <yannick.jadoul@belgacom.net>
2020-09-15 16:24:39 +0200

    Change base parameter type in register_exception and exception constructor from PyObject* to handle (#2467)
    * Change base parameter type in register_exception and excepion constructor from PyObject* to handle
    
    * Fix compilation error passing `handle` to `PyObject*`

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -1868,10 +1868,10 @@ template <typename type>
 class exception : public object {
 public:
     exception() = default;
-    exception(handle scope, const char *name, PyObject *base = PyExc_Exception) {
+    exception(handle scope, const char *name, handle base = PyExc_Exception) {
         std::string full_name = scope.attr("__name__").cast<std::string>() +
                                 std::string(".") + name;
-        m_ptr = PyErr_NewException(const_cast<char *>(full_name.c_str()), base, NULL);
+        m_ptr = PyErr_NewException(const_cast<char *>(full_name.c_str()), base.ptr(), NULL);
         if (hasattr(scope, name))
             pybind11_fail("Error during initialization: multiple incompatible "
                           "definitions with name \"" + std::string(name) + "\"");
@@ -1901,7 +1901,7 @@ PYBIND11_NAMESPACE_END(detail)
 template <typename CppException>
 exception<CppException> &register_exception(handle scope,
                                             const char *name,
-                                            PyObject *base = PyExc_Exception) {
+                                            handle base = PyExc_Exception) {
     auto &ex = detail::get_exception_object<CppException>();
     if (!ex) ex = exception<CppException>(scope, name, base);
 
