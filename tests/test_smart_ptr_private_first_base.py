9623045b7b163caba1c2855fcbca692fbf216c38
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2020-12-22 12:18:47 -0800

    Additional demonstration of Undefined Behavior in handling of shared_ptr holder.

--- tests/test_smart_ptr_private_first_base.py
+++ tests/test_smart_ptr_private_first_base.py
@@ -3,7 +3,16 @@ import pytest
 
 from pybind11_tests import smart_ptr_private_first_base as m
 
-def test_make_pass():
+
+def test_make_drvd_pass_base():
     d = m.make_shared_drvd()
     i = m.pass_shared_base(d)
+    assert i == 200
+
+
+def test_make_drvd_up_cast_pass_drvd():
+    b = m.make_shared_drvd_up_cast()
+    # the base return is up-cast immediately.
+    assert b.__class__.__name__ == "drvd"
+    i = m.pass_shared_drvd(b)
     assert i == 200
