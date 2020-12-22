fbf70a0143124d5aa052a56f6d156afef90cfe61
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2020-12-22 12:18:47 -0800

    Additional demonstration of Undefined Behavior in handling of shared_ptr holder.

--- tests/test_smart_ptr_private_first_base.cpp
+++ tests/test_smart_ptr_private_first_base.cpp
@@ -3,8 +3,12 @@
 // https://github.com/pybind/pybind11/blob/30eb39ed79d1e2eeff15219ac00773034300a5e6/include/pybind11/cast.h#L235
 //     `return reinterpret_cast<H &>(vh[1]);`
 // indirectly casts a `shared_ptr<drvd>` reference to a `shared_ptr<base>`.
-// `test_smart_ptr_private_first_base.py` fails with an AssertionError and
-// a subsequent Segmentation Fault (Linux, clang++ -std=c++17).
+// Similarly:
+// https://github.com/pybind/pybind11/blob/30eb39ed79d1e2eeff15219ac00773034300a5e6/include/pybind11/pybind11.h#L1505
+//     `init_holder(inst, v_h, (const holder_type *) holder_ptr, v_h.value_ptr<type>());`
+// explictly casts a `shared_ptr<base>` reference to a `shared_ptr<drvd>`.
+// Both tests in `test_smart_ptr_private_first_base.py` fail with a
+// Segmentation Fault (Linux, clang++ -std=c++17).
 
 #include <memory>
 
@@ -33,14 +37,21 @@ inline std::shared_ptr<drvd> make_shared_drvd() {
   return std::shared_ptr<drvd>(new drvd);
 }
 
+inline std::shared_ptr<base> make_shared_drvd_up_cast() {
+  return std::shared_ptr<base>(new drvd);
+}
+
 inline int pass_shared_base(std::shared_ptr<base> b) { return b->id(); }
+inline int pass_shared_drvd(std::shared_ptr<drvd> d) { return d->id(); }
 
 TEST_SUBMODULE(smart_ptr_private_first_base, m) {
   py::class_<base, std::shared_ptr<base>>(m, "base");
   py::class_<drvd, base, std::shared_ptr<drvd>>(m, "drvd");
 
   m.def("make_shared_drvd", make_shared_drvd);
+  m.def("make_shared_drvd_up_cast", make_shared_drvd_up_cast);
   m.def("pass_shared_base", pass_shared_base);
+  m.def("pass_shared_drvd", pass_shared_drvd);
 }
 
 }  // namespace smart_ptr_private_first_base
