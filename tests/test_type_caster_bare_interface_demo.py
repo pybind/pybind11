de10541b1218d4f1747f0d5e3ed9dcfbd0fbb995
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-01-10 12:22:23 -0800

    Adding type_caster_bare_interface_demo.

---
+++ tests/test_type_caster_bare_interface_demo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from pybind11_tests import type_caster_bare_interface_demo as m
+
+
+def test_cast():
+    assert m.rtrn_mpty_valu() == "cast_rref"
+    assert m.rtrn_mpty_rref() == "cast_rref"
+    assert m.rtrn_mpty_cref() == "cast_cref"
+    assert m.rtrn_mpty_mref() == "cast_mref"
+    assert m.rtrn_mpty_cptr() == "cast_cptr"
+    assert m.rtrn_mpty_mptr() == "cast_mptr"
+
+
+def test_load():
+    assert m.pass_mpty_valu(None) == "load_valu"
+    assert m.pass_mpty_rref(None) == "load_rref"
+    assert m.pass_mpty_cref(None) == "load_cref"
+    assert m.pass_mpty_mref(None) == "load_mref"
+    assert m.pass_mpty_cptr(None) == "load_cptr"
+    assert m.pass_mpty_mptr(None) == "load_mptr"
