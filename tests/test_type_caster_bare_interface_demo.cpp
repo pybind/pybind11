1c9b8ebb804df42f91027d3476b2c411b646fad2
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-01-10 13:09:30 -0800

    Adding shared_ptr<mpty>, shared_ptr<mpty const> casters.

--- tests/test_type_caster_bare_interface_demo.cpp
+++ tests/test_type_caster_bare_interface_demo.cpp
@@ -1,5 +1,7 @@
 #include "pybind11_tests.h"
 
+#include <memory>
+
 namespace pybind11_tests {
 namespace type_caster_bare_interface_demo {
 
@@ -18,6 +20,12 @@ const char* pass_mpty_cref(mpty const&) { return "load_cref"; }
 const char* pass_mpty_mref(mpty&)       { return "load_mref"; }
 const char* pass_mpty_cptr(mpty const*) { return "load_cptr"; }
 const char* pass_mpty_mptr(mpty*)       { return "load_mptr"; }
+
+std::shared_ptr<mpty>       rtrn_mpty_shmp() { return std::shared_ptr<mpty>(new mpty); }
+std::shared_ptr<mpty const> rtrn_mpty_shcp() { return std::shared_ptr<mpty const>(new mpty); }
+
+const char* pass_mpty_shmp(std::shared_ptr<mpty>)       { return "load_shmp"; }
+const char* pass_mpty_shcp(std::shared_ptr<mpty const>) { return "load_shcp"; }
 
 }  // namespace type_caster_bare_interface_demo
 }  // namespace pybind11_tests
@@ -66,11 +74,11 @@ struct type_caster<mpty> {
 
     template <typename T_>
     using cast_op_type = conditional_t<
-        std::is_same<remove_reference_t<T_>, const mpty*>::value, const mpty*,
+        std::is_same<remove_reference_t<T_>, mpty const*>::value, mpty const*,
         conditional_t<
             std::is_same<remove_reference_t<T_>, mpty*>::value, mpty*,
             conditional_t<
-                std::is_same<T_, const mpty&>::value, const mpty&,
+                std::is_same<T_, mpty const&>::value, mpty const&,
                 conditional_t<
                     std::is_same<T_, mpty&>::value, mpty&,
                     conditional_t<
@@ -85,7 +93,45 @@ struct type_caster<mpty> {
     operator mpty*()       { return rtrn_mpty_mptr(); }
 
     bool load(handle /*src*/, bool /*convert*/) {
-      return true;
+        return true;
+    }
+};
+
+template <>
+struct type_caster<std::shared_ptr<mpty>> {
+    static constexpr auto name = _<std::shared_ptr<mpty>>();
+
+    static handle cast(const std::shared_ptr<mpty>& /*src*/,
+                       return_value_policy /*policy*/,
+                       handle /*parent*/) {
+        return str("cast_shmp").release();
+    }
+
+    template <typename> using cast_op_type = std::shared_ptr<mpty>;
+
+    operator std::shared_ptr<mpty>() { return rtrn_mpty_shmp(); }
+
+    bool load(handle /*src*/, bool /*convert*/) {
+        return true;
+    }
+};
+
+template <>
+struct type_caster<std::shared_ptr<mpty const>> {
+    static constexpr auto name = _<std::shared_ptr<mpty const>>();
+
+    static handle cast(const std::shared_ptr<mpty const>& /*src*/,
+                       return_value_policy /*policy*/,
+                       handle /*parent*/) {
+        return str("cast_shcp").release();
+    }
+
+    template <typename> using cast_op_type = std::shared_ptr<mpty const>;
+
+    operator std::shared_ptr<mpty const>() { return rtrn_mpty_shcp(); }
+
+    bool load(handle /*src*/, bool /*convert*/) {
+        return true;
     }
 };
 
@@ -96,19 +142,25 @@ namespace pybind11_tests {
 namespace type_caster_bare_interface_demo {
 
 TEST_SUBMODULE(type_caster_bare_interface_demo, m) {
-  m.def("rtrn_mpty_valu", rtrn_mpty_valu);
-  m.def("rtrn_mpty_rref", rtrn_mpty_rref);
-  m.def("rtrn_mpty_cref", rtrn_mpty_cref);
-  m.def("rtrn_mpty_mref", rtrn_mpty_mref);
-  m.def("rtrn_mpty_cptr", rtrn_mpty_cptr);
-  m.def("rtrn_mpty_mptr", rtrn_mpty_mptr);
+    m.def("rtrn_mpty_valu", rtrn_mpty_valu);
+    m.def("rtrn_mpty_rref", rtrn_mpty_rref);
+    m.def("rtrn_mpty_cref", rtrn_mpty_cref);
+    m.def("rtrn_mpty_mref", rtrn_mpty_mref);
+    m.def("rtrn_mpty_cptr", rtrn_mpty_cptr);
+    m.def("rtrn_mpty_mptr", rtrn_mpty_mptr);
 
-  m.def("pass_mpty_valu", pass_mpty_valu);
-  m.def("pass_mpty_rref", pass_mpty_rref);
-  m.def("pass_mpty_cref", pass_mpty_cref);
-  m.def("pass_mpty_mref", pass_mpty_mref);
-  m.def("pass_mpty_cptr", pass_mpty_cptr);
-  m.def("pass_mpty_mptr", pass_mpty_mptr);
+    m.def("pass_mpty_valu", pass_mpty_valu);
+    m.def("pass_mpty_rref", pass_mpty_rref);
+    m.def("pass_mpty_cref", pass_mpty_cref);
+    m.def("pass_mpty_mref", pass_mpty_mref);
+    m.def("pass_mpty_cptr", pass_mpty_cptr);
+    m.def("pass_mpty_mptr", pass_mpty_mptr);
+
+    m.def("rtrn_mpty_shmp", rtrn_mpty_shmp);
+    m.def("rtrn_mpty_shcp", rtrn_mpty_shcp);
+
+    m.def("pass_mpty_shmp", pass_mpty_shmp);
+    m.def("pass_mpty_shcp", pass_mpty_shcp);
 }
 
 }  // namespace type_caster_bare_interface_demo
