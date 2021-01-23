202577dbe3259be47019299f12255c8ab40ae2b3
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-01-23 08:27:03 -0800

    Pure renaming of function names in test_classh_inheritance, similar to the systematic approach used in test_class_wip. NO functional changes.

--- tests/test_classh_inheritance.py
+++ tests/test_classh_inheritance.py
@@ -3,36 +3,36 @@
 from pybind11_tests import classh_inheritance as m
 
 
-def test_make_drvd_pass_base():
-    d = m.make_drvd()
-    i = m.pass_base(d)  # load_impl Case 2a
+def test_rtrn_mptr_drvd_pass_cptr_base():
+    d = m.rtrn_mptr_drvd()
+    i = m.pass_cptr_base(d)  # load_impl Case 2a
     assert i == 2 * 100 + 11
 
 
-def test_make_drvd_up_cast_pass_drvd():
-    b = m.make_drvd_up_cast()
+def test_rtrn_mptr_drvd_up_cast_pass_cptr_drvd():
+    b = m.rtrn_mptr_drvd_up_cast()
     # the base return is down-cast immediately.
     assert b.__class__.__name__ == "drvd"
-    i = m.pass_drvd(b)
+    i = m.pass_cptr_drvd(b)
     assert i == 2 * 100 + 12
 
 
-def test_make_drvd2_pass_bases():
-    d = m.make_drvd2()
-    i1 = m.pass_base1(d)  # load_impl Case 2c
+def test_rtrn_mptr_drvd2_pass_cptr_bases():
+    d = m.rtrn_mptr_drvd2()
+    i1 = m.pass_cptr_base1(d)  # load_impl Case 2c
     assert i1 == 3 * 110 + 4 * 120 + 21
-    i2 = m.pass_base2(d)
+    i2 = m.pass_cptr_base2(d)
     assert i2 == 3 * 110 + 4 * 120 + 22
 
 
-def test_make_drvd2_up_casts_pass_drvd2():
-    b1 = m.make_drvd2_up_cast1()
+def test_rtrn_mptr_drvd2_up_casts_pass_cptr_drvd2():
+    b1 = m.rtrn_mptr_drvd2_up_cast1()
     assert b1.__class__.__name__ == "drvd2"
-    i1 = m.pass_drvd2(b1)
+    i1 = m.pass_cptr_drvd2(b1)
     assert i1 == 3 * 110 + 4 * 120 + 23
-    b2 = m.make_drvd2_up_cast2()
+    b2 = m.rtrn_mptr_drvd2_up_cast2()
     assert b2.__class__.__name__ == "drvd2"
-    i2 = m.pass_drvd2(b2)
+    i2 = m.pass_cptr_drvd2(b2)
     assert i2 == 3 * 110 + 4 * 120 + 23
 
 
@@ -43,7 +43,7 @@ def test_python_drvd2():
             m.base2.__init__(self)
 
     d = Drvd2()
-    i1 = m.pass_base1(d)  # load_impl Case 2b
+    i1 = m.pass_cptr_base1(d)  # load_impl Case 2b
     assert i1 == 110 + 21
-    i2 = m.pass_base2(d)
+    i2 = m.pass_cptr_base2(d)
     assert i2 == 120 + 22
