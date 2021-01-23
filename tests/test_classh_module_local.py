6e825d33408022d991225edf4aa9d969fc25665f
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-01-23 08:49:48 -0800

    Adopting systematic naming scheme from test_classh_wip. NO functional changes.

--- tests/test_classh_module_local.py
+++ tests/test_classh_module_local.py
@@ -6,22 +6,22 @@ import classh_module_local_1 as m1
 import classh_module_local_2 as m2
 
 
-def test_cross_module_get_msg():
-    b1 = m1.bottle("A")
-    assert b1.tag() == 1
-    b2 = m2.bottle("B")
-    assert b2.tag() == 2
-    assert m1.get_msg(b1) == "A"
-    assert m2.get_msg(b2) == "B"
-    assert m1.get_msg(b2) == "B"
-    assert m2.get_msg(b1) == "A"
-    assert m0.get_msg(b1) == "A"
-    assert m0.get_msg(b2) == "B"
+def test_cross_module_get_mtxt():
+    obj1 = m1.atyp("A")
+    assert obj1.tag() == 1
+    obj2 = m2.atyp("B")
+    assert obj2.tag() == 2
+    assert m1.get_mtxt(obj1) == "A"
+    assert m2.get_mtxt(obj2) == "B"
+    assert m1.get_mtxt(obj2) == "B"
+    assert m2.get_mtxt(obj1) == "A"
+    assert m0.get_mtxt(obj1) == "A"
+    assert m0.get_mtxt(obj2) == "B"
 
 
-def test_m0_make_bottle():
+def test_m0_rtrn_valu_atyp():
     with pytest.raises(TypeError) as exc_info:
-        m0.make_bottle()
+        m0.rtrn_valu_atyp()
     assert str(exc_info.value).startswith(
         "Unable to convert function return value to a Python type!"
     )
