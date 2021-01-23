235fa288b8e092bbe55a6e76e69c7e96599fae47
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-01-23 08:39:26 -0800

    Pure renaming of function and variable names, for better generalization when convoluting with inheritance. NO functional changes.

--- tests/test_classh_wip.py
+++ tests/test_classh_wip.py
@@ -4,65 +4,65 @@ import pytest
 from pybind11_tests import classh_wip as m
 
 
-def test_mpty_constructors():
-    e = m.mpty()
-    assert e.__class__.__name__ == "mpty"
-    e = m.mpty("")
-    assert e.__class__.__name__ == "mpty"
-    e = m.mpty("txtm")
-    assert e.__class__.__name__ == "mpty"
+def test_atyp_constructors():
+    e = m.atyp()
+    assert e.__class__.__name__ == "atyp"
+    e = m.atyp("")
+    assert e.__class__.__name__ == "atyp"
+    e = m.atyp("txtm")
+    assert e.__class__.__name__ == "atyp"
 
 
 def test_cast():
-    assert m.get_mtxt(m.rtrn_mpty_valu()) == "rtrn_valu"
-    assert m.get_mtxt(m.rtrn_mpty_rref()) == "rtrn_rref"
-    assert m.get_mtxt(m.rtrn_mpty_cref()) == "rtrn_cref"
-    assert m.get_mtxt(m.rtrn_mpty_mref()) == "rtrn_mref"
-    assert m.get_mtxt(m.rtrn_mpty_cptr()) == "rtrn_cptr"
-    assert m.get_mtxt(m.rtrn_mpty_mptr()) == "rtrn_mptr"
+    assert m.get_mtxt(m.rtrn_valu_atyp()) == "rtrn_valu"
+    assert m.get_mtxt(m.rtrn_rref_atyp()) == "rtrn_rref"
+    assert m.get_mtxt(m.rtrn_cref_atyp()) == "rtrn_cref"
+    assert m.get_mtxt(m.rtrn_mref_atyp()) == "rtrn_mref"
+    assert m.get_mtxt(m.rtrn_cptr_atyp()) == "rtrn_cptr"
+    assert m.get_mtxt(m.rtrn_mptr_atyp()) == "rtrn_mptr"
 
 
 def test_load():
-    assert m.pass_mpty_valu(m.mpty("Valu")) == "pass_valu:Valu"
-    assert m.pass_mpty_rref(m.mpty("Rref")) == "pass_rref:Rref"
-    assert m.pass_mpty_cref(m.mpty("Cref")) == "pass_cref:Cref"
-    assert m.pass_mpty_mref(m.mpty("Mref")) == "pass_mref:Mref"
-    assert m.pass_mpty_cptr(m.mpty("Cptr")) == "pass_cptr:Cptr"
-    assert m.pass_mpty_mptr(m.mpty("Mptr")) == "pass_mptr:Mptr"
+    assert m.pass_valu_atyp(m.atyp("Valu")) == "pass_valu:Valu"
+    assert m.pass_rref_atyp(m.atyp("Rref")) == "pass_rref:Rref"
+    assert m.pass_cref_atyp(m.atyp("Cref")) == "pass_cref:Cref"
+    assert m.pass_mref_atyp(m.atyp("Mref")) == "pass_mref:Mref"
+    assert m.pass_cptr_atyp(m.atyp("Cptr")) == "pass_cptr:Cptr"
+    assert m.pass_mptr_atyp(m.atyp("Mptr")) == "pass_mptr:Mptr"
 
 
 def test_cast_shared_ptr():
-    assert m.get_mtxt(m.rtrn_mpty_shmp()) == "rtrn_shmp"
-    assert m.get_mtxt(m.rtrn_mpty_shcp()) == "rtrn_shcp"
+    assert m.get_mtxt(m.rtrn_shmp_atyp()) == "rtrn_shmp"
+    assert m.get_mtxt(m.rtrn_shcp_atyp()) == "rtrn_shcp"
 
 
 def test_load_shared_ptr():
-    assert m.pass_mpty_shmp(m.mpty("Shmp")) == "pass_shmp:Shmp"
-    assert m.pass_mpty_shcp(m.mpty("Shcp")) == "pass_shcp:Shcp"
+    assert m.pass_shmp_atyp(m.atyp("Shmp")) == "pass_shmp:Shmp"
+    assert m.pass_shcp_atyp(m.atyp("Shcp")) == "pass_shcp:Shcp"
 
 
 def test_cast_unique_ptr():
-    assert m.get_mtxt(m.rtrn_mpty_uqmp()) == "rtrn_uqmp"
-    assert m.get_mtxt(m.rtrn_mpty_uqcp()) == "rtrn_uqcp"
+    assert m.get_mtxt(m.rtrn_uqmp_atyp()) == "rtrn_uqmp"
+    assert m.get_mtxt(m.rtrn_uqcp_atyp()) == "rtrn_uqcp"
 
 
 def test_load_unique_ptr():
-    assert m.pass_mpty_uqmp(m.mpty("Uqmp")) == "pass_uqmp:Uqmp"
-    assert m.pass_mpty_uqcp(m.mpty("Uqcp")) == "pass_uqcp:Uqcp"
+    assert m.pass_uqmp_atyp(m.atyp("Uqmp")) == "pass_uqmp:Uqmp"
+    assert m.pass_uqcp_atyp(m.atyp("Uqcp")) == "pass_uqcp:Uqcp"
 
 
 @pytest.mark.parametrize(
-    "pass_mpty, argm, rtrn",
+    "pass_atyp, argm, rtrn",
     [
-        (m.pass_mpty_uqmp, "Uqmp", "pass_uqmp:Uqmp"),
-        (m.pass_mpty_uqcp, "Uqcp", "pass_uqcp:Uqcp"),
+        (m.pass_uqmp_atyp, "Uqmp", "pass_uqmp:Uqmp"),
+        (m.pass_uqcp_atyp, "Uqcp", "pass_uqcp:Uqcp"),
     ],
 )
-def test_pass_unique_ptr_disowns(pass_mpty, argm, rtrn):
-    obj = m.mpty(argm)
-    assert pass_mpty(obj) == rtrn
+def test_pass_unique_ptr_disowns(pass_atyp, argm, rtrn):
+    obj = m.atyp(argm)
+    assert pass_atyp(obj) == rtrn
     with pytest.raises(RuntimeError) as exc_info:
-        m.pass_mpty_uqmp(obj)
+        m.pass_uqmp_atyp(obj)
     assert str(exc_info.value) == (
         "Missing value for wrapped C++ type:"
         " Python instance is uninitialized or was disowned."
@@ -71,7 +71,7 @@ def test_pass_unique_ptr_disowns(pass_mpty, argm, rtrn):
 
 def test_unique_ptr_roundtrip(num_round_trips=1000):
     # Multiple roundtrips to stress-test instance registration/deregistration.
-    recycled = m.mpty("passenger")
+    recycled = m.atyp("passenger")
     for _ in range(num_round_trips):
         id_orig = id(recycled)
         recycled = m.unique_ptr_roundtrip(recycled)
