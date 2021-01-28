098630efc4c09b4c9289e92b6dc68e46f07b4e07
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-01-28 07:31:40 -0800

    Reintroducing py::classh, this time as a simple alias for py::class_<U, py::smart_holder>.

--- include/pybind11/smart_holder.h
+++ include/pybind11/smart_holder.h
@@ -5,3 +5,18 @@
 #pragma once
 
 #include "detail/smart_holder_type_casters.h"
+#include "pybind11.h"
+
+PYBIND11_NAMESPACE_BEGIN(PYBIND11_NAMESPACE)
+
+// Supports easier switching between py::class_<U> and py::class_<U, py::smart_holder>:
+// users can simply replace the `_` in `class_` with `h` or vice versa.
+// Note though that the PYBIND11_SMART_HOLDER_TYPE_CASTERS(U) macro also needs to be
+// added (for `classh`) or commented out (for `class_`).
+template <typename type_, typename... options>
+class classh : public class_<type_, smart_holder, options...> {
+public:
+    using class_<type_, smart_holder, options...>::class_;
+};
+
+PYBIND11_NAMESPACE_END(PYBIND11_NAMESPACE)
