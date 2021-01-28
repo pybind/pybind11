c7210a20186acb5e0bdc01b1f8b7aa007b4fe196
Edward Lockhart <locked@google.com>
2021-01-28 10:37:45 +0000

    weak_from_this is only available in C++17 and later

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -1505,14 +1505,22 @@ private:
     template <typename T>
     static void init_holder(detail::instance *inst, detail::value_and_holder &v_h,
             const holder_type * /* unused */, const std::enable_shared_from_this<T> * /* dummy */) {
+#if defined(PYBIND11_CPP17)
         if (!v_h.value_ptr<type>()->weak_from_this().expired()) {
+#else
+        try {
+#endif
             auto sh = std::dynamic_pointer_cast<typename holder_type::element_type>(
                     v_h.value_ptr<type>()->shared_from_this());
             if (sh) {
                 new (std::addressof(v_h.holder<holder_type>())) holder_type(std::move(sh));
                 v_h.set_holder_constructed();
             }
+#if defined(PYBIND11_CPP17)
         }
+#else
+        } catch (const std::bad_weak_ptr &) {}
+#endif
 
         if (!v_h.holder_constructed() && inst->owned) {
             new (std::addressof(v_h.holder<holder_type>())) holder_type(v_h.value_ptr<type>());
