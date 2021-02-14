53c10649dd12a11a84bb5e28a18df8857aaab042
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-02-14 10:40:26 -0800

    Adding construct() overloads for constructing smart_holder from alias unique_ptr, shared_ptr returns.

--- tests/test_class_sh_factory_constructors.py
+++ tests/test_class_sh_factory_constructors.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import pytest
 
 from pybind11_tests import class_sh_factory_constructors as m
 
@@ -16,3 +17,36 @@ def test_atyp_factories():
     # sert m.atyp_uqcp().get_mtxt() == "Uqcp"
     assert m.atyp_udmp().get_mtxt() == "Udmp"
     # sert m.atyp_udcp().get_mtxt() == "Udcp"
+
+
+@pytest.mark.parametrize(
+    "init_args, expected",
+    [
+        ((3,), 300),
+        ((5, 7), 570),
+        ((9, 11, 13), 1023),
+    ],
+)
+def test_with_alias_success(init_args, expected):
+    assert m.with_alias(*init_args).val == expected
+
+
+@pytest.mark.parametrize(
+    "num_init_args, smart_ptr",
+    [
+        (4, "std::unique_ptr"),
+        (5, "std::shared_ptr"),
+    ],
+)
+def test_with_alias_invalid(num_init_args, smart_ptr):
+    class PyDrvdWithAlias(m.with_alias):
+        pass
+
+    with pytest.raises(TypeError) as excinfo:
+        PyDrvdWithAlias(*((0,) * num_init_args))
+    assert (
+        str(excinfo.value)
+        == "pybind11::init(): construction failed: returned "
+        + smart_ptr
+        + " pointee is not an alias instance"
+    )
