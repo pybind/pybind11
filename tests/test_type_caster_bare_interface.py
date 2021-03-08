5d21d8a74f43b6addef44541533c3a6e939ba377
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-03-08 11:38:40 -0800

    Tracing CpCtor, MvCtor, removing smart-pointer casters, renaming, more comments.

--- tests/test_type_caster_bare_interface.py
+++ tests/test_type_caster_bare_interface.py
@@ -1,41 +1,36 @@
 # -*- coding: utf-8 -*-
+import re
+
+import pytest
 
 from pybind11_tests import type_caster_bare_interface as m
 
 
-def test_cast():
-    assert m.rtrn_valu() == "cast_rref"
-    assert m.rtrn_rref() == "cast_rref"
-    assert m.rtrn_cref() == "cast_cref"
-    assert m.rtrn_mref() == "cast_mref"
-    assert m.rtrn_cptr() == "cast_cptr"
-    assert m.rtrn_mptr() == "cast_mptr"
+@pytest.mark.parametrize(
+    "rtrn_f, expected",
+    [
+        (m.rtrn_valu, "cast_rref:valu_CpCtor"),
+        (m.rtrn_rref, "cast_rref:rref"),
+        (m.rtrn_cref, "cast_cref:cref"),
+        (m.rtrn_mref, "cast_mref:mref"),
+        (m.rtrn_cptr, "cast_cptr:cptr"),
+        (m.rtrn_mptr, "cast_mptr:mptr"),
+    ],
+)
+def test_cast(rtrn_f, expected):
+    assert re.match(expected, rtrn_f())
 
 
-def test_load():
-    assert m.pass_valu(None) == "load_valu"
-    assert m.pass_rref(None) == "load_rref"
-    assert m.pass_cref(None) == "load_cref"
-    assert m.pass_mref(None) == "load_mref"
-    assert m.pass_cptr(None) == "load_cptr"
-    assert m.pass_mptr(None) == "load_mptr"
-
-
-def test_cast_shared_ptr():
-    assert m.rtrn_shmp() == "cast_shmp"
-    assert m.rtrn_shcp() == "cast_shcp"
-
-
-def test_load_shared_ptr():
-    assert m.pass_shmp(None) == "load_shmp"
-    assert m.pass_shcp(None) == "load_shcp"
-
-
-def test_cast_unique_ptr():
-    assert m.rtrn_uqmp() == "cast_uqmp"
-    assert m.rtrn_uqcp() == "cast_uqcp"
-
-
-def test_load_unique_ptr():
-    assert m.pass_uqmp(None) == "load_uqmp"
-    assert m.pass_uqcp(None) == "load_uqcp"
+@pytest.mark.parametrize(
+    "pass_f, expected",
+    [
+        (m.pass_valu, "pass_valu:rref_MvCtor"),
+        (m.pass_rref, "pass_rref:rref"),
+        (m.pass_cref, "pass_cref:cref"),
+        (m.pass_mref, "pass_mref:mref"),
+        (m.pass_cptr, "pass_cptr:cptr"),
+        (m.pass_mptr, "pass_mptr:mptr"),
+    ],
+)
+def test_operator(pass_f, expected):
+    assert re.match(expected, pass_f(None))
