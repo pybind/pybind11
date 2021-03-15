08745af59f20093eed1e737f8572e1e927c83300
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-03-15 11:45:10 -0700

    Restoring test for exception for the case that virtual_overrider_self_life_support is not used.

--- tests/test_class_sh_with_alias.py
+++ tests/test_class_sh_with_alias.py
@@ -1,35 +1,57 @@
 # -*- coding: utf-8 -*-
+import pytest
 
 from pybind11_tests import class_sh_with_alias as m
 
 
-class PyDrvd(m.Abase):
+class PyDrvd0(m.Abase0):
     def __init__(self, val):
-        super(PyDrvd, self).__init__(val)
+        super(PyDrvd0, self).__init__(val)
 
     def Add(self, other_val):  # noqa:  N802
         return self.Get() * 100 + other_val
 
 
-def test_drvd_add():
-    drvd = PyDrvd(74)
+class PyDrvd1(m.Abase1):
+    def __init__(self, val):
+        super(PyDrvd1, self).__init__(val)
+
+    def Add(self, other_val):  # noqa:  N802
+        return self.Get() * 200 + other_val
+
+
+def test_drvd0_add():
+    drvd = PyDrvd0(74)
     assert drvd.Add(38) == (74 * 10 + 3) * 100 + 38
 
 
-def test_add_in_cpp_raw_ptr():
-    drvd = PyDrvd(52)
+def test_drvd0_add_in_cpp_raw_ptr():
+    drvd = PyDrvd0(52)
     assert m.AddInCppRawPtr(drvd, 27) == ((52 * 10 + 3) * 100 + 27) * 10 + 7
 
 
-def test_add_in_cpp_shared_ptr():
+def test_drvd0_add_in_cpp_shared_ptr():
     while True:
-        drvd = PyDrvd(36)
+        drvd = PyDrvd0(36)
         assert m.AddInCppSharedPtr(drvd, 56) == ((36 * 10 + 3) * 100 + 56) * 100 + 11
         return  # Comment out for manual leak checking (use `top` command).
 
 
-def test_add_in_cpp_unique_ptr():
+def test_drvd0_add_in_cpp_unique_ptr():
     while True:
-        drvd = PyDrvd(25)
-        assert m.AddInCppUniquePtr(drvd, 83) == ((25 * 10 + 3) * 100 + 83) * 100 + 13
+        drvd = PyDrvd0(0)
+        with pytest.raises(ValueError) as exc_info:
+            m.AddInCppUniquePtr(drvd, 0)
+        assert (
+            str(exc_info.value)
+            == "Ownership of instance with virtual overrides in Python"
+            " cannot be transferred to C++."
+        )
+        return  # Comment out for manual leak checking (use `top` command).
+
+
+def test_drvd1_add_in_cpp_unique_ptr():
+    while True:
+        drvd = PyDrvd1(25)
+        assert m.AddInCppUniquePtr(drvd, 83) == ((25 * 10 + 3) * 200 + 83) * 100 + 13
         return  # Comment out for manual leak checking (use `top` command).
