65f562f6662e3bc866e4f0ad8330315734d59b6f
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-03-21 10:38:11 -0700

    Replacing py::overload_cast with plain cast for C++11 compatibility.

--- tests/test_class_sh_disowning.cpp
+++ tests/test_class_sh_disowning.cpp
@@ -41,6 +41,6 @@ TEST_SUBMODULE(class_sh_disowning, m) {
 
     m.def("mixed", mixed);
 
-    m.def("overloaded", py::overload_cast<std::unique_ptr<Atype<1>>, int>(&overloaded));
-    m.def("overloaded", py::overload_cast<std::unique_ptr<Atype<2>>, int>(&overloaded));
+    m.def("overloaded", (int (*)(std::unique_ptr<Atype<1>>, int)) & overloaded);
+    m.def("overloaded", (int (*)(std::unique_ptr<Atype<2>>, int)) & overloaded);
 }
