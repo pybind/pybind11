64e47cd1ae5196fd15f49d22d531a6589d4a2f5b
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-03-26 09:02:22 -0700

    Adding PyGILState_Check() in object_api<>::operator().

--- include/pybind11/cast.h
+++ include/pybind11/cast.h
@@ -1348,6 +1348,11 @@ unpacking_collector<policy> collect_arguments(Args &&...args) {
 template <typename Derived>
 template <return_value_policy policy, typename... Args>
 object object_api<Derived>::operator()(Args &&...args) const {
+#if PY_VERSION_HEX >= 0x03040000
+    if (!PyGILState_Check()) {
+        pybind11_fail("pybind11::object_api<>::operator() PyGILState_Check() failure.");
+    }
+#endif
     return detail::collect_arguments<policy>(std::forward<Args>(args)...).call(derived().ptr());
 }
 
