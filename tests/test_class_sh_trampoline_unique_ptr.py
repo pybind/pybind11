34c7ad0680b9df1f7a8c2dc0930ddbf356697dcc
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-04-08 17:49:08 -0700

    Fixing trivial formatting issue.

--- tests/test_class_sh_trampoline_unique_ptr.py
+++ tests/test_class_sh_trampoline_unique_ptr.py
@@ -4,12 +4,11 @@ import pybind11_tests.class_sh_trampoline_unique_ptr as m
 
 
 class MyClass(m.Class):
+    def foo(self):
+        return 10
 
-  def foo(self):
-    return 10
-
-  def clone(self):
-    return MyClass()
+    def clone(self):
+        return MyClass()
 
 
 def test_py_clone_and_foo():
