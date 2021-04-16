46bd53278c521831f71234907f84d7a05125491c
mvoelkle-cern <54059203+mvoelkle-cern@users.noreply.github.com>
2021-04-16 11:26:53 +0200

    Fix compilation with gcc < 5
    When the user defines _GLIBCXX_USE_CXX11_ABI=0 to force the pre-c++11 ABI, numpy.h assumes that is_trivially_copyable is available.
    It is not necessarily the case. This patch uses clang's feature detection instead.

--- include/pybind11/numpy.h
+++ include/pybind11/numpy.h
@@ -319,12 +319,10 @@ template <typename T> using remove_all_extents_t = typename array_info<T>::type;
 
 template <typename T> using is_pod_struct = all_of<
     std::is_standard_layout<T>,     // since we're accessing directly in memory we need a standard layout type
-#if !defined(__GNUG__) || defined(_LIBCPP_VERSION) || defined(_GLIBCXX_USE_CXX11_ABI)
-    // _GLIBCXX_USE_CXX11_ABI indicates that we're using libstdc++ from GCC 5 or newer, independent
-    // of the actual compiler (Clang can also use libstdc++, but it always defines __GNUC__ == 4).
+#if !defined(__GNUC__) || __GNUC__ >= 5 || (defined(__clang__) && __has_feature(is_trivially_copyable))
     std::is_trivially_copyable<T>,
 #else
-    // GCC 4 doesn't implement is_trivially_copyable, so approximate it
+    // libstdc++ < 5 doesn't implement is_trivially_copyable, so approximate it
     std::is_trivially_destructible<T>,
     satisfies_any_of<T, std::has_trivial_copy_constructor, std::has_trivial_copy_assign>,
 #endif
