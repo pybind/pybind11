54ac1b9c52f939d118bca9a386dab65656c82f1c
Jack S. Hale <mail@jackhale.co.uk>
2021-06-07 16:26:13 +0200

    See if existing test triggers for a const type.

--- tests/test_numpy_dtypes.cpp
+++ tests/test_numpy_dtypes.cpp
@@ -31,7 +31,7 @@ std::ostream& operator<<(std::ostream& os, const SimpleStruct& v) {
 
 struct SimpleStructReordered {
     bool bool_;
-    float float_;
+    const float float_;
     uint32_t uint_;
     long double ldbl_;
 };
