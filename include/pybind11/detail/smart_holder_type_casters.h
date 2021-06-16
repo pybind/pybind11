4fc34e2eca852f24d98890adf0add3671c9704e6
Jakob Lykke Andersen <Jakob@caput.dk>
2021-06-16 15:42:51 +0200

    Allow copy policy in smart holder caster for shared_ptr

--- include/pybind11/detail/smart_holder_type_casters.h
+++ include/pybind11/detail/smart_holder_type_casters.h
@@ -625,6 +625,7 @@ struct smart_holder_type_caster<std::shared_ptr<T>> : smart_holder_type_caster_l
     static handle cast(const std::shared_ptr<T> &src, return_value_policy policy, handle parent) {
         if (policy != return_value_policy::automatic
             && policy != return_value_policy::reference_internal
+            && policy != return_value_policy::copy
             && policy != return_value_policy::automatic_reference) {
             // SMART_HOLDER_WIP: IMPROVABLE: Error message.
             throw cast_error("Invalid return_value_policy for shared_ptr.");
