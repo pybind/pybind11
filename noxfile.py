85523831a95664558300eb25c4fb8272baa328cd
Henry Schreiner <henryschreineriii@gmail.com>
2021-07-13 14:30:47 -0400

    docs: try using Furo

--- noxfile.py
+++ noxfile.py
@@ -53,14 +53,17 @@ def docs(session: nox.Session) -> None:
 
     session.install("-r", "docs/requirements.txt")
     session.chdir("docs")
-    session.run("sphinx-build", "-M", "html", ".", "_build")
+    if "pdf" in session.posargs:
+        session.run("sphinx-build", "-M", "latexpdf", ".", "_build")
+        return
+    else:
+        session.run("sphinx-build", "-M", "html", ".", "_build")
 
-    if session.posargs:
-        if "serve" in session.posargs:
-            print("Launching docs at http://localhost:8000/ - use Ctrl-C to quit")
-            session.run("python", "-m", "http.server", "8000", "-d", "_build/html")
-        else:
-            print("Unsupported argument to docs")
+    if "serve" in session.posargs:
+        print("Launching docs at http://localhost:8000/ - use Ctrl-C to quit")
+        session.run("python", "-m", "http.server", "8000", "-d", "_build/html")
+    elif session.posargs:
+        print("Unsupported argument to docs")
 
 
 @nox.session(reuse_venv=True)
