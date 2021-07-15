4592c5e1f4c1d7055bc7b148b49e87e9fdf20add
Henry Schreiner <henryschreineriii@gmail.com>
2021-07-15 15:39:46 -0400

    docs: darker output

--- docs/conf.py
+++ docs/conf.py
@@ -155,7 +155,11 @@ html_theme = "furo"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ["_static"]
+
+html_css_files = [
+    "css/custom.css",
+]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
