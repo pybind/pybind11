c3e43aad4ceb68cff24aabbf687b0905b5e787b1
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-07-15 12:54:43 -0700

    Also reporting REPEAT_COMMAND:RETCODE_COUNTS

--- tools/repeat_command.py
+++ tools/repeat_command.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import collections
 import subprocess
 import sys
 
@@ -15,7 +16,7 @@ def run(args):
     assert cmd_and_args
     print("REPEAT_COMMAND:CMD_AND_ARGS", cmd_and_args)
     print()
-    sys.stdout.flush()
+    retcode_counts = collections.defaultdict(int)
     first_non_zero_retcode = 0
     for ix in range(num_repeats):
         print("REPEAT_COMMAND:CALL", ix + 1)
@@ -23,9 +24,10 @@ def run(args):
         retcode = subprocess.call(cmd_and_args)
         print("REPEAT_COMMAND:RETCODE", retcode)
         print()
-        sys.stdout.flush()
+        retcode_counts[retcode] += 1
         if retcode and not first_non_zero_retcode:
             first_non_zero_retcode = retcode
+    print("REPEAT_COMMAND:RETCODE_COUNTS", list(sorted(retcode_counts.items())))
     print("REPEAT_COMMAND:FIRST_NON_ZERO_RETCODE", first_non_zero_retcode)
     print()
     sys.stdout.flush()
