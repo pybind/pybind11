16bf9691e26925cfeb3fefbdf2f83c2f13d533ea
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-07-16 20:57:14 -0700

    Removing pragma ICC 878

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -13,7 +13,7 @@
 #if defined(__INTEL_COMPILER)
 #  pragma warning push
 #  pragma warning disable 68    // integer conversion resulted in a change of sign
-#  pragma warning disable 878   // incompatible exception specifications
+#  pragma warning disable 186   // pointless comparison of unsigned integer with zero
 #  pragma warning disable 1334  // the "template" keyword used for syntactic disambiguation may only be used within a template
 #  pragma warning disable 1682  // implicit conversion of a 64-bit integral type to a smaller integral type (potential portability problem)
 #  pragma warning disable 1786  // function "strdup" was declared deprecated
