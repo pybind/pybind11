75948c988631be5416be6cd601c5b3c44c41e6ef
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-07-16 20:58:21 -0700

    Removing pragma ICC 1786

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -16,7 +16,7 @@
 #  pragma warning disable 186   // pointless comparison of unsigned integer with zero
 #  pragma warning disable 878   // incompatible exception specifications
 #  pragma warning disable 1334  // the "template" keyword used for syntactic disambiguation may only be used within a template
-#  pragma warning disable 1786  // function "strdup" was declared deprecated
+#  pragma warning disable 1682  // implicit conversion of a 64-bit integral type to a smaller integral type (potential portability problem)
 #  pragma warning disable 1875  // offsetof applied to non-POD (Plain Old Data) types is nonstandard
 #  pragma warning disable 2196  // warning #2196: routine is both "inline" and "noinline"
 #elif defined(_MSC_VER)
