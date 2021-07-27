1f418c0cf5481046a6f14bf1e8c5910e8cee44c9
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-07-26 23:41:31 -0700

    Revising #ifdef to be more conservative.

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -13,7 +13,7 @@
 //
 // THE `push` HERE NEED TO BE KEPT IN SYNC WITH THE CORRESPONDING `pop` AT THE BOTTOM OF THIS FILE.
 //
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
 #  pragma warning(push)
 #  pragma warning(disable: 4100) // warning C4100: Unreferenced formal parameter
 #  pragma warning(disable: 4127) // warning C4127: Conditional expression is constant
@@ -2378,7 +2378,7 @@ PYBIND11_NAMESPACE_END(PYBIND11_NAMESPACE)
 //
 // THE `pop` HERE NEED TO BE KEPT IN SYNC WITH THE CORRESPONDING `push` AT THE TOP OF THIS FILE.
 //
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
 #  pragma warning(pop)
 #elif defined(__GNUG__) && !defined(__clang__) && !defined(__INTEL_COMPILER)
 #  pragma GCC diagnostic pop
