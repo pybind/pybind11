65765885eb191608c17b1dd263e3ab0d10aa3651
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-07-27 17:44:14 -0700

    Removing pragma for 4100 (to see what is still broken with the latest code).

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -12,7 +12,6 @@
 
 #if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
 #  pragma warning(push)
-#  pragma warning(disable: 4100) // warning C4100: Unreferenced formal parameter
 #  pragma warning(disable: 4127) // warning C4127: Conditional expression is constant
 #  pragma warning(disable: 4505) // warning C4505: 'PySlice_GetIndicesEx': unreferenced local function has been removed (PyPy only)
 #elif defined(__GNUG__) && !defined(__clang__) && !defined(__INTEL_COMPILER)
