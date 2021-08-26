cb60ed49e46fb31cc578b86b2178d8ce574617ad
Ye Zhihao <vigilans@foxmail.com>
2021-08-27 05:34:24 +0800

    Fix enum value's __int__ returning non-int when underlying type is bool or of char type (#1334)
    * Use equivalent_integer for enum's Scalar decision
    
    * Add test for char underlying enum
    
    * Support translating bool type in enum's Scalar
    
    * Add test for bool underlying enum
    
    * Fix comment in test
    
    * Switch from `PYBIND11_CPP20` macro to `PYBIND11_HAS_U8STRING`
    
    * Refine tests
    
    Co-authored-by: Aaron Gokaslan <skylion.aaron@gmail.com>

--- tests/test_enum.py
+++ tests/test_enum.py
@@ -218,16 +218,44 @@ def test_binary_operators():
 def test_enum_to_int():
     m.test_enum_to_int(m.Flags.Read)
     m.test_enum_to_int(m.ClassWithUnscopedEnum.EMode.EFirstMode)
+    m.test_enum_to_int(m.ScopedCharEnum.Positive)
+    m.test_enum_to_int(m.ScopedBoolEnum.TRUE)
     m.test_enum_to_uint(m.Flags.Read)
     m.test_enum_to_uint(m.ClassWithUnscopedEnum.EMode.EFirstMode)
+    m.test_enum_to_uint(m.ScopedCharEnum.Positive)
+    m.test_enum_to_uint(m.ScopedBoolEnum.TRUE)
     m.test_enum_to_long_long(m.Flags.Read)
     m.test_enum_to_long_long(m.ClassWithUnscopedEnum.EMode.EFirstMode)
+    m.test_enum_to_long_long(m.ScopedCharEnum.Positive)
+    m.test_enum_to_long_long(m.ScopedBoolEnum.TRUE)
 
 
 def test_duplicate_enum_name():
     with pytest.raises(ValueError) as excinfo:
         m.register_bad_enum()
     assert str(excinfo.value) == 'SimpleEnum: element "ONE" already exists!'
+
+
+def test_char_underlying_enum():  # Issue #1331/PR #1334:
+    assert type(m.ScopedCharEnum.Positive.__int__()) is int
+    assert int(m.ScopedChar16Enum.Zero) == 0  # int() call should successfully return
+    assert hash(m.ScopedChar32Enum.Positive) == 1
+    assert m.ScopedCharEnum.Positive.__getstate__() == 1  # return type is long in py2.x
+    assert m.ScopedWCharEnum(1) == m.ScopedWCharEnum.Positive
+    with pytest.raises(TypeError):
+        # Enum should construct with a int, even with char underlying type
+        m.ScopedWCharEnum("0")
+
+
+def test_bool_underlying_enum():
+    assert type(m.ScopedBoolEnum.TRUE.__int__()) is int
+    assert int(m.ScopedBoolEnum.FALSE) == 0
+    assert hash(m.ScopedBoolEnum.TRUE) == 1
+    assert m.ScopedBoolEnum.TRUE.__getstate__() == 1
+    assert m.ScopedBoolEnum(1) == m.ScopedBoolEnum.TRUE
+    # Enum could construct with a bool
+    # (bool is a strict subclass of int, and False will be converted to 0)
+    assert m.ScopedBoolEnum(False) == m.ScopedBoolEnum.FALSE
 
 
 def test_docstring_signatures():
