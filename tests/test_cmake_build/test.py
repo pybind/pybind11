e9eb2c8b6cec4cc13d0275b16589ef1f0880c466
Aaron Gokaslan <skylion.aaron@gmail.com>
2021-09-01 16:17:02 -0400

    Guard test with Python version

--- tests/test_cmake_build/test.py
+++ tests/test_cmake_build/test.py
@@ -3,7 +3,10 @@ import sys
 
 import test_cmake_build
 
-assert isinstance(__file__, str)  # Test this is properly set
+from .. import env
+
+if env.PY3:
+    assert isinstance(__file__, str)  # Test this is properly set
 
 assert test_cmake_build.add(1, 2) == 3
 print("{} imports, runs, and adds: 1 + 2 = 3".format(sys.argv[1]))
