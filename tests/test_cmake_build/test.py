3e340898c3bf67612ae49c5c3fccd54d93c4b720
Aaron Gokaslan <skylion.aaron@gmail.com>
2021-09-01 17:00:23 -0400

    Hack

--- tests/test_cmake_build/test.py
+++ tests/test_cmake_build/test.py
@@ -3,9 +3,7 @@ import sys
 
 import test_cmake_build
 
-from .. import env
-
-if env.PY3:
+if str is not bytes:  # If not Python2
     assert isinstance(__file__, str)  # Test this is properly set
 
 assert test_cmake_build.add(1, 2) == 3
