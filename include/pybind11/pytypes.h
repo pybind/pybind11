f8f2fa059a1b7a5e9a5edd86a27093f0935fda0b
Aaron Gokaslan <skylion.aaron@gmail.com>
2021-09-02 13:20:28 -0400

    Revert exception ctor

--- include/pybind11/pytypes.h
+++ include/pybind11/pytypes.h
@@ -336,7 +336,7 @@ public:
     }
 
     error_already_set(const error_already_set &) = default;
-    error_already_set(error_already_set &&) noexcept = default;
+    error_already_set(error_already_set &&) = default;
 
     inline ~error_already_set() override;
 
