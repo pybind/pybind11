804b69a3683ded98e1c25f3b161aff67c1329908
Pavel Panteliuk <pavel-panteluk@mail.ru>
2021-11-19 12:39:41 +0300

    fix compiler warning: deprecated implicit copy constructor

--- include/pybind11/eigen.h
+++ include/pybind11/eigen.h
@@ -81,13 +81,11 @@ template <bool EigenRowMajor> struct EigenConformable {
     // Matrix type:
     EigenConformable(EigenIndex r, EigenIndex c,
             EigenIndex rstride, EigenIndex cstride) :
-        conformable{true}, rows{r}, cols{c} {
+        conformable{true}, rows{r}, cols{c}, stride{EigenRowMajor ? rstride : cstride /* outer stride */,
+                        EigenRowMajor ? cstride : rstride /* inner stride */ } {
         // TODO: when Eigen bug #747 is fixed, remove the tests for non-negativity. http://eigen.tuxfamily.org/bz/show_bug.cgi?id=747
         if (rstride < 0 || cstride < 0) {
             negativestrides = true;
-        } else {
-            stride = {EigenRowMajor ? rstride : cstride /* outer stride */,
-                      EigenRowMajor ? cstride : rstride /* inner stride */ };
         }
     }
     // Vector type:
