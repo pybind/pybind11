50be0c5d0ceb55ebd5a6a0de08707ec8abc55db8
Pavel Panteliuk <pavel-panteluk@mail.ru>
2021-11-22 17:01:22 +0300

    add parenthesis for better reading

--- include/pybind11/eigen.h
+++ include/pybind11/eigen.h
@@ -81,12 +81,11 @@ template <bool EigenRowMajor> struct EigenConformable {
     EigenConformable(EigenIndex r, EigenIndex c,
             EigenIndex rstride, EigenIndex cstride) :
         conformable{true}, rows{r}, cols{c},
-        stride{EigenRowMajor ? rstride > 0 ? rstride : 0 : cstride > 0 ? cstride : 0 /* outer stride */,
-               EigenRowMajor ? cstride > 0 ? cstride : 0 : rstride > 0 ? rstride : 0 /* inner stride */ } {
-        // TODO: when Eigen bug #747 is fixed, remove the tests for non-negativity. http://eigen.tuxfamily.org/bz/show_bug.cgi?id=747
-        if (rstride < 0 || cstride < 0) {
-            negativestrides = true;
-        }
+        //TODO: when Eigen bug #747 is fixed, remove the tests for non-negativity. http://eigen.tuxfamily.org/bz/show_bug.cgi?id=747
+        negativestrides{rstride < 0 || cstride < 0},
+        stride{EigenRowMajor ? (rstride > 0 ? rstride : 0) : (cstride > 0 ? cstride : 0) /* outer stride */,
+               EigenRowMajor ? (cstride > 0 ? cstride : 0) : (rstride > 0 ? rstride : 0) /* inner stride */ } {
+        
     }
     // Vector type:
     EigenConformable(EigenIndex r, EigenIndex c, EigenIndex stride)
