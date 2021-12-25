f5b4000b4f5f420ada0909fdf6cf27926d703cf0
Steve R. Sun <1638650145@qq.com>
2021-12-25 13:20:05 +0800

    Replace _ with const_name

--- include/pybind11/numpy.h
+++ include/pybind11/numpy.h
@@ -285,23 +285,23 @@ struct npy_format_descriptor_name;
 
 template <typename T>
 struct npy_format_descriptor_name<T, enable_if_t<std::is_integral<T>::value>> {
-    static constexpr auto name = _<std::is_same<T, bool>::value>(
-        _("bool"), _<std::is_signed<T>::value>("int", "uint") + _<sizeof(T)*8>()
+    static constexpr auto name = const_name<std::is_same<T, bool>::value>(
+        const_name("bool"), const_name<std::is_signed<T>::value>("int", "uint") + const_name<sizeof(T)*8>()
     );
 };
 
 template <typename T>
 struct npy_format_descriptor_name<T, enable_if_t<std::is_floating_point<T>::value>> {
-    static constexpr auto name = _<std::is_same<T, float>::value || std::is_same<T, double>::value>(
-        _("float") + _<sizeof(T)*8>(), _("longdouble")
+    static constexpr auto name = const_name<std::is_same<T, float>::value || std::is_same<T, double>::value>(
+        const_name("float") + const_name<sizeof(T)*8>(), const_name("longdouble")
     );
 };
 
 template <typename T>
 struct npy_format_descriptor_name<T, enable_if_t<is_complex<T>::value>> {
-static constexpr auto name = _<std::is_same<typename T::value_type, float>::value
+static constexpr auto name = const_name<std::is_same<typename T::value_type, float>::value
                                || std::is_same<typename T::value_type, double>::value>(
-        _("complex") + _<sizeof(typename T::value_type)*16>(), _("longcomplex")
+        const_name("complex") + const_name<sizeof(typename T::value_type)*16>(), const_name("longcomplex")
 );
 };
 
