407acfeba87b3b58d8e0493f5efff1bb4cee32bd
Henry Schreiner <HenrySchreinerIII@gmail.com>
2021-12-28 14:32:29 -0500

    fix: support conda-forge

--- setup.py
+++ setup.py
@@ -146,6 +146,8 @@ with remove_output("pybind11/include", "pybind11/share"):
             "-DBUILD_TESTING=OFF",
             "-DPYBIND11_NOPYTHON=ON",
         ]
+        if "CMAKE_ARGS" in os.environ:
+            cmd += os.environ["CMAKE_ARGS"].split()
         cmake_opts = dict(cwd=DIR, stdout=sys.stdout, stderr=sys.stderr)
         subprocess.check_call(cmd, **cmake_opts)
         subprocess.check_call(["cmake", "--install", tmpdir], **cmake_opts)
