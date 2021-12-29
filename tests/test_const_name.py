f1491bb864969a34b2ceeefd541e4a7314fdc34d
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-12-28 17:23:04 -0800

    Stacking @pytest.mark.parametrize (thanks to @skylion007 for pointing out).

--- tests/test_const_name.py
+++ tests/test_const_name.py
@@ -1,35 +1,30 @@
 # -*- coding: utf-8 -*-
-import itertools
-
 import pytest
 
 import env
 from pybind11_tests import const_name as m
 
 
+@pytest.mark.parametrize("func", (m.const_name_tests, m.underscore_tests))
 @pytest.mark.parametrize(
-    "func, selector_expected",
-    itertools.product(
-        (m.const_name_tests, m.underscore_tests),
-        enumerate(
-            (
-                "",
-                "A",
-                "Bd",
-                "Cef",
-                "%",
-                "%",
-                "T1",
-                "U2",
-                "D1",
-                "E2",
-                "KeepAtEnd",
-            )
-        ),
+    "selector, expected",
+    enumerate(
+        (
+            "",
+            "A",
+            "Bd",
+            "Cef",
+            "%",
+            "%",
+            "T1",
+            "U2",
+            "D1",
+            "E2",
+            "KeepAtEnd",
+        )
     ),
 )
-def test_const_name(func, selector_expected):
-    selector, expected = selector_expected
+def test_const_name(func, selector, expected):
     if isinstance(func, type(u"") if env.PY2 else str):
         pytest.skip(func)
     text = func(selector)
