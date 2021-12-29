6082ec4bfa7d273cd3d6735091691b76230651c9
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-12-29 12:48:10 -0800

    Adding link to PyCLIF test this was reduced from.

--- tests/test_class_sh_property.py
+++ tests/test_class_sh_property.py
@@ -2,6 +2,8 @@
 from pybind11_tests import class_sh_property as m
 
 
+# Reduced from:
+# https://github.com/google/clif/blob/c371a6d4b28d25d53a16e6d2a6d97305fb1be25a/clif/testing/python/nested_fields_test.py#L56
 def test_inner_access_after_disowning_outer():
     outer = m.Outer()
     inner = outer.field
