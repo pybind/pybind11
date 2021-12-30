9b9e1bb13d52ae4a26311bd03eb9dc5219a983e3
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2021-12-30 07:51:53 -0800

    Using aliasing shared_ptr in field getter.
    Emulating PyCLIF approach:
    https://github.com/google/clif/blob/c371a6d4b28d25d53a16e6d2a6d97305fb1be25a/clif/python/instance.h#L233

--- tests/test_class_sh_property.cpp
+++ tests/test_class_sh_property.cpp
@@ -25,12 +25,21 @@ TEST_SUBMODULE(class_sh_property, m) {
     using namespace test_class_sh_property;
 
     py::classh<Inner>(m, "Inner")              //
+        .def(py::init<>())                     //
         .def_readwrite("value", &Inner::value) //
         ;
 
-    py::classh<Outer>(m, "Outer")              //
-        .def(py::init<>())                     //
-        .def_readwrite("field", &Outer::field) //
+    py::classh<Outer>(m, "Outer") //
+        .def(py::init<>())        //
+        .def_property(
+            "field", //
+            [](const std::shared_ptr<Outer> &self) {
+                // Emulating PyCLIF approach:
+                // https://github.com/google/clif/blob/c371a6d4b28d25d53a16e6d2a6d97305fb1be25a/clif/python/instance.h#L233
+                return std::shared_ptr<Inner>(self, &self->field);
+            },                                                          //
+            [](Outer &self, const Inner &field) { self.field = field; } //
+            )                                                           //
         ;
 
     m.def("DisownOuter", DisownOuter);
--- tests/test_class_sh_property.py
+++ tests/test_class_sh_property.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
+import pytest
+
 from pybind11_tests import class_sh_property as m
 
 
-# Reduced from:
-# https://github.com/google/clif/blob/c371a6d4b28d25d53a16e6d2a6d97305fb1be25a/clif/testing/python/nested_fields_test.py#L56
-def test_inner_access_after_disowning_outer():
+def test_field_getter(msg):
+    # Reduced from PyCLIF test:
+    # https://github.com/google/clif/blob/c371a6d4b28d25d53a16e6d2a6d97305fb1be25a/clif/testing/python/nested_fields_test.py#L56
     outer = m.Outer()
     inner = outer.field
     assert inner.value == -99
+    with pytest.raises(ValueError) as excinfo:
+        m.DisownOuter(outer)
+    assert msg(excinfo.value) == "Cannot disown use_count != 1 (loaded_as_unique_ptr)."
+    del inner
     m.DisownOuter(outer)
-    # assert inner.value == -99 # AddressSanitizer: heap-use-after-free
+    with pytest.raises(ValueError) as excinfo:
+        outer.field
+    assert (
+        msg(excinfo.value)
+        == "Missing value for wrapped C++ type: Python instance was disowned."
+    )
+
+
+def test_field_setter(msg):
+    outer = m.Outer()
+    assert outer.field.value == -99
+    field = m.Inner()
+    field.value = 35
+    outer.field = field
+    assert outer.field.value == 35
