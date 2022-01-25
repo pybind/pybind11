e7d6c8be494cf2db7bd1e93e9c115dbecb005194
Amaury Forgeot d'Arc <amauryfa@gmail.com>
2022-01-26 00:28:08 +0100

    Fix compilation.

--- include/pybind11/cast.h
+++ include/pybind11/cast.h
@@ -999,15 +999,15 @@ template <typename T> enable_if_t<!cast_is_temporary_value_reference<T>::value, 
 // though if it's in dead code, so we provide a "trampoline" to pybind11::cast that only does anything in
 // cases where pybind11::cast is valid.
 template <typename T>
-enable_if_t<!std::is_same<void, intrinsic_t<T>> &&
+enable_if_t<!std::is_same<void, intrinsic_t<T>>::value &&
             !cast_is_temporary_value_reference<T>::value, T>
 cast_safe(object &&o) { return pybind11::cast<T>(std::move(o)); }
 template <typename T>
 enable_if_t<cast_is_temporary_value_reference<T>::value, T>
 cast_safe(object &&) {  pybind11_fail("Internal error: cast_safe fallback invoked"); }
 template <typename T>
-enable_if_t<std::is_same<void, intrinsic_t<T>>, void>
-cast_safe<void>(object &&) {}
+enable_if_t<std::is_same<void, intrinsic_t<T>>::value, void>
+cast_safe(object &&) {}
 
 PYBIND11_NAMESPACE_END(detail)
 
