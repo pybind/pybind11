ba794a52d3d1decb04cba2b340db4847547162e9
Aaron Gokaslan <skylion.aaron@gmail.com>
2022-01-28 12:28:50 -0500

    Clarify comment

--- include/pybind11/detail/internals.h
+++ include/pybind11/detail/internals.h
@@ -201,7 +201,8 @@ struct type_info {
     void *(*module_local_load)(PyObject *, const type_info *) = nullptr;
     /* A simple type never occurs as a (direct or indirect) parent
      * of a class that makes use of multiple inheritance.
-     * The child remains simple unless it becomes a parent of a pybind type. */
+     * A type can be simple even if it has non-simple ancestors as long as it has no descendants.
+     */
     bool simple_type : 1;
     /* True if there is no multiple inheritance in this type's inheritance tree */
     bool simple_ancestors : 1;
