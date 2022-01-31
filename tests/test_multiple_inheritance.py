3a8d92308d3291e21c55baca8aa7b8dd5b3a8d56
Aaron Gokaslan <aaronGokaslan@gmail.com>
2022-01-31 12:19:48 -0500

    Fix caster optimization regression introduced in #3650 (#3659)
    * Fix optimization bug introduced in #3650
    
    * Add simple Python extension test for MVF
    
    * Improve comments
    
    * Clarify comment
    
    * Clarify another comment
    
    * Add test docstring
    
    * Fix typo

--- tests/test_multiple_inheritance.py
+++ tests/test_multiple_inheritance.py
@@ -472,3 +472,25 @@ def test_pr3635_diamond_f():
     assert o.get_f_e() == 5
 
     assert o.get_f_f() == 6
+
+
+def test_python_inherit_from_mi():
+    """Tests extending a Python class from a single inheritor of a MI class"""
+
+    class PyMVF(m.MVF):
+        g = 7
+
+        def get_g_g(self):
+            return self.g
+
+    o = PyMVF()
+
+    assert o.b == 1
+    assert o.c == 2
+    assert o.d0 == 3
+    assert o.d1 == 4
+    assert o.e == 5
+    assert o.f == 6
+    assert o.g == 7
+
+    assert o.get_g_g() == 7
