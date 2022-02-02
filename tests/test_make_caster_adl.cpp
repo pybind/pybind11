7d2e5242f21f09221b1b9cfd185172d3cd581e9a
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-02-01 17:43:18 -0800

    Adding test_minimal_real_caster. Keeping only one mock_caster.

--- tests/test_make_caster_adl.cpp
+++ tests/test_make_caster_adl.cpp
@@ -1,22 +1,56 @@
 #include "pybind11_tests.h"
 
-namespace adl_one {
-struct type_one {};
-struct type_caster_one {
+// adl = Argument Dependent Lookup
+
+namespace adl_mock {
+struct type_mock {};
+struct mock_caster {
     static int num() { return 101; }
 };
-type_caster_one pybind11_select_caster(type_one *);
-} // namespace adl_one
+mock_caster pybind11_select_caster(type_mock *);
+} // namespace adl_mock
 
-namespace adl_two {
-struct type_two {};
-struct type_caster_two {
-    static int num() { return 202; }
+namespace adl_mrc { // minimal real caster
+
+struct type_mrc {
+    int value = -9999;
 };
-type_caster_two pybind11_select_caster(type_two *);
-} // namespace adl_two
+
+struct minimal_real_caster {
+    static constexpr auto name = py::detail::const_name<type_mrc>();
+
+    static py::handle
+    cast(type_mrc const &src, py::return_value_policy /*policy*/, py::handle /*parent*/) {
+        return py::int_(src.value + 1000).release();
+    }
+
+    // Maximizing simplicity. This will go terribly wrong for other arg types.
+    template <typename>
+    using cast_op_type = const type_mrc &;
+
+    operator type_mrc const &() {
+        static type_mrc obj;
+        obj.value = 303;
+        return obj;
+    }
+
+    bool load(py::handle src, bool /*convert*/) {
+        // Only accepts str, but the value is ignored.
+        return py::isinstance<py::str>(src);
+    }
+};
+
+minimal_real_caster pybind11_select_caster(type_mrc *);
+
+} // namespace adl_mrc
 
 TEST_SUBMODULE(make_caster_adl, m) {
-    m.def("num_one", []() { return py::detail::make_caster<adl_one::type_one>::num(); });
-    m.def("num_two", []() { return py::detail::make_caster<adl_two::type_two>::num(); });
+    m.def("num_mock", []() { return py::detail::make_caster<adl_mock::type_mock>::num(); });
+
+    m.def("obj_mrc_return", []() {
+        adl_mrc::type_mrc obj;
+        obj.value = 404;
+        return obj;
+    });
+    m.def("obj_mrc_arg", [](adl_mrc::type_mrc const &obj) { return obj.value + 2000; });
 }
--- tests/test_make_caster_adl.py
+++ tests/test_make_caster_adl.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
+import pytest
 
 from pybind11_tests import make_caster_adl as m
 
 
-def test_basic():
-    assert m.num_one() == 101
-    assert m.num_two() == 202
+def test_mock_caster():
+    assert m.num_mock() == 101
+
+
+def test_minimal_real_caster():
+    assert m.obj_mrc_return() == 1404
+    assert m.obj_mrc_arg("ignored") == 2303
+    with pytest.raises(TypeError) as excinfo:
+        m.obj_mrc_arg(None)
+    assert "(arg0: adl_mrc::type_mrc) -> int" in str(excinfo.value)
