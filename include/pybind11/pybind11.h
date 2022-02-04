768614b7c26319e6f0ef632a19fbc274c04a02d1
Aaron Gokaslan <skylion.aaron@gmail.com>
2022-02-04 14:43:22 -0500

    test 3.11 proper frame access

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -2384,19 +2384,19 @@ inline function get_type_override(const void *this_ptr, const type_info *this_ty
 
     /* Don't call dispatch code if invoked from overridden function.
        Unfortunately this doesn't work on PyPy. */
-#if !defined(PYPY_VERSION) && PY_VERSION_HEX < 0x030B0000
+#if !defined(PYPY_VERSION)
     // TODO: Remove PyPy workaround for Python 3.11.
     // Current API fails on 3.11 since co_varnames can be null.
-#if PY_VERSION_HEX >= 0x03090000
+#if PY_VERSION_HEX >= 0x030B0000
     PyFrameObject *frame = PyThreadState_GetFrame(PyThreadState_Get());
     if (frame != nullptr) {
         PyCodeObject *f_code = PyFrame_GetCode(frame);
         // f_code is guaranteed to not be NULL
         if ((std::string) str(f_code->co_name) == name && f_code->co_argcount > 0) {
-            PyObject* locals = PyEval_GetLocals();
+            PyObject* locals = PyObject_GetAttrString(frame, "f_locals");
             if (locals != nullptr && f_code->co_varnames != nullptr) {
                 PyObject *self_caller = dict_getitem(
-                    locals, PyTuple_GET_ITEM(f_code->co_varnames, 0)
+                    locals, PyTuple_GET_ITEM(PyObject_GetAttrString(f_code, "co_varnames"), 0)
                 );
                 if (self_caller == self.ptr()) {
                     Py_DECREF(f_code);
