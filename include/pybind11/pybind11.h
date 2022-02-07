a0eb05b4e336d83b396ed667667be36e0e06ad61
Aaron Gokaslan <skylion.aaron@gmail.com>
2022-02-07 11:20:40 -0500

    Test out Python 3.11 migration

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -2384,7 +2384,7 @@ inline function get_type_override(const void *this_ptr, const type_info *this_ty
 
     /* Don't call dispatch code if invoked from overridden function.
        Unfortunately this doesn't work on PyPy. */
-#if !defined(PYPY_VERSION) && PY_VERSION_HEX < 0x030B0000
+#if !defined(PYPY_VERSION)
     // TODO: Remove PyPy workaround for Python 3.11.
     // Current API fails on 3.11 since co_varnames can be null.
 #if PY_VERSION_HEX >= 0x03090000
@@ -2394,10 +2394,11 @@ inline function get_type_override(const void *this_ptr, const type_info *this_ty
         // f_code is guaranteed to not be NULL
         if ((std::string) str(f_code->co_name) == name && f_code->co_argcount > 0) {
             PyObject* locals = PyEval_GetLocals();
-            if (locals != nullptr && f_code->co_varnames != nullptr) {
+            if (locals != nullptr) {
                 PyObject *self_caller = dict_getitem(
-                    locals, PyTuple_GET_ITEM(f_code->co_varnames, 0)
-                );
+                    locals,
+                    PyTuple_GET_ITEM(PyObject_GetAttrString((PyObject *) f_code, "co_varnames"),
+                                     0));
                 if (self_caller == self.ptr()) {
                     Py_DECREF(f_code);
                     Py_DECREF(frame);
