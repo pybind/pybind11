d07865846c7d31dd61111e6df801864327b65070
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-02-11 20:37:09 -0800

    After clang-format (via pre-commit).

--- tests/test_const_name.cpp
+++ tests/test_const_name.cpp
@@ -5,44 +5,44 @@
 #include "pybind11_tests.h"
 
 // IUT = Implementation Under Test
-#    define CONST_NAME_TESTS(TEST_FUNC, IUT)                                                      \
-        std::string TEST_FUNC(int selector) {                                                     \
-            switch (selector) {                                                                   \
-                case 0:                                                                           \
-                    return IUT("").text;                                                          \
-                case 1:                                                                           \
-                    return IUT("A").text;                                                         \
-                case 2:                                                                           \
-                    return IUT("Bd").text;                                                        \
-                case 3:                                                                           \
-                    return IUT("Cef").text;                                                       \
-                case 4:                                                                           \
-                    return IUT<int>().text; /*NOLINT(bugprone-macro-parentheses)*/                \
-                case 5:                                                                           \
-                    return IUT<std::string>().text; /*NOLINT(bugprone-macro-parentheses)*/        \
-                case 6:                                                                           \
-                    return IUT<true>("T1", "T2").text; /*NOLINT(bugprone-macro-parentheses)*/     \
-                case 7:                                                                           \
-                    return IUT<false>("U1", "U2").text; /*NOLINT(bugprone-macro-parentheses)*/    \
-                case 8:                                                                           \
-                    /*NOLINTNEXTLINE(bugprone-macro-parentheses)*/                                \
-                    return IUT<true>(IUT("D1"), IUT("D2")).text;                                  \
-                case 9:                                                                           \
-                    /*NOLINTNEXTLINE(bugprone-macro-parentheses)*/                                \
-                    return IUT<false>(IUT("E1"), IUT("E2")).text;                                 \
-                case 10:                                                                          \
-                    return IUT("KeepAtEnd").text;                                                 \
-                default:                                                                          \
-                    break;                                                                        \
-            }                                                                                     \
-            throw std::runtime_error("Invalid selector value.");                                  \
-        }
+#define CONST_NAME_TESTS(TEST_FUNC, IUT)                                                          \
+    std::string TEST_FUNC(int selector) {                                                         \
+        switch (selector) {                                                                       \
+            case 0:                                                                               \
+                return IUT("").text;                                                              \
+            case 1:                                                                               \
+                return IUT("A").text;                                                             \
+            case 2:                                                                               \
+                return IUT("Bd").text;                                                            \
+            case 3:                                                                               \
+                return IUT("Cef").text;                                                           \
+            case 4:                                                                               \
+                return IUT<int>().text; /*NOLINT(bugprone-macro-parentheses)*/                    \
+            case 5:                                                                               \
+                return IUT<std::string>().text; /*NOLINT(bugprone-macro-parentheses)*/            \
+            case 6:                                                                               \
+                return IUT<true>("T1", "T2").text; /*NOLINT(bugprone-macro-parentheses)*/         \
+            case 7:                                                                               \
+                return IUT<false>("U1", "U2").text; /*NOLINT(bugprone-macro-parentheses)*/        \
+            case 8:                                                                               \
+                /*NOLINTNEXTLINE(bugprone-macro-parentheses)*/                                    \
+                return IUT<true>(IUT("D1"), IUT("D2")).text;                                      \
+            case 9:                                                                               \
+                /*NOLINTNEXTLINE(bugprone-macro-parentheses)*/                                    \
+                return IUT<false>(IUT("E1"), IUT("E2")).text;                                     \
+            case 10:                                                                              \
+                return IUT("KeepAtEnd").text;                                                     \
+            default:                                                                              \
+                break;                                                                            \
+        }                                                                                         \
+        throw std::runtime_error("Invalid selector value.");                                      \
+    }
 
 CONST_NAME_TESTS(const_name_tests, py::detail::const_name)
 
-#    ifdef PYBIND11_DETAIL_UNDERSCORE_BACKWARD_COMPATIBILITY
+#ifdef PYBIND11_DETAIL_UNDERSCORE_BACKWARD_COMPATIBILITY
 CONST_NAME_TESTS(underscore_tests, py::detail::_)
-#    endif
+#endif
 
 TEST_SUBMODULE(const_name, m) {
     m.def("const_name_tests", const_name_tests);
