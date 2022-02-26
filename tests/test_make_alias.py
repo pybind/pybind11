a14c936562d859f610c9718897d6afe330b8bf25
Tom de Geus <tdegeus@users.noreply.github.com>
2022-02-26 22:37:44 +0100

    Update tests/test_make_alias.py
    Co-authored-by: Sebastian Koslowski <sebastian.koslowski@gmail.com>

--- tests/test_make_alias.py
+++ tests/test_make_alias.py
@@ -2,7 +2,7 @@ import pytest
 
 from pybind11_tests import ConstructorStats
 
-m = pytest.importorskip("pybind11_tests.make_alias")
+m = pytest.importorskip("pybind11_tests._make_alias")
 
 
 def assert_name(mat):
