9b9df3d588ac18a95184c3488f6c483beb3bff13
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-03-01 14:39:08 -0500

    Update include/pybind11/pybind11.h

--- include/pybind11/pybind11.h
+++ include/pybind11/pybind11.h
@@ -2645,9 +2645,11 @@ get_type_override(const void *this_ptr, const type_info *this_type, const char *
         if ((std::string) str(f_code->co_name) == name && f_code->co_argcount > 0) {
             PyObject *locals = PyEval_GetLocals();
             if (locals != nullptr) {
-                PyObject *self_arg = PyTuple_GET_ITEM(
-                    PyObject_GetAttrString((PyObject *) f_code, "co_varnames"), 0);
+                PyObject *co_varnames = 
+                    PyObject_GetAttrString((PyObject *) f_code, "co_varnames");
+                PyObject *self_arg = PyTuple_GET_ITEM(co_varnames, 0);
                 PyObject *self_caller = dict_getitem(locals, self_arg);
+                Py_DECREF(co_varnames);
                 if (self_caller == self.ptr()) {
                     Py_DECREF(f_code);
                     Py_DECREF(frame);
