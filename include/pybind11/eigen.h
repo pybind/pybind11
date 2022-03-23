8ca47c5218817dbfbd8a94b21a9c62b6df98c510
Aaron Gokaslan <skylion.aaron@gmail.com>
2022-03-23 11:00:54 -0700

    Add a couple more std::move

--- include/pybind11/eigen.h
+++ include/pybind11/eigen.h
@@ -528,8 +528,8 @@ public:
 
         ref.reset();
         map.reset(new MapType(data(copy_or_ref),
-                              fits.rows,
-                              fits.cols,
+                              std::move(fits.rows),
+                              std::move(fits.cols),
                               make_stride(fits.stride.outer(), fits.stride.inner())));
         ref.reset(new Type(*map));
 
@@ -668,7 +668,7 @@ struct type_caster<Type, enable_if_t<is_eigen_sparse<Type>::value>> {
                                      Type::Flags &(Eigen::RowMajor | Eigen::ColMajor),
                                      StorageIndex>(shape[0].cast<Index>(),
                                                    shape[1].cast<Index>(),
-                                                   nnz,
+                                                   std::move(nnz),
                                                    outerIndices.mutable_data(),
                                                    innerIndices.mutable_data(),
                                                    values.mutable_data());
