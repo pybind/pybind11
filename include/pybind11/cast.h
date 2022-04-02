126fc7c524ea7a51b54720defd75de3470d69557
Aaron Gokaslan <skylion.aaron@gmail.com>
2022-04-02 14:14:53 -0400

    Add missing move in arg_v constructor

--- include/pybind11/cast.h
+++ include/pybind11/cast.h
@@ -1243,8 +1243,8 @@ struct arg_v : arg {
 private:
     template <typename T>
     arg_v(arg &&base, T &&x, const char *descr = nullptr)
-        : arg(base), value(reinterpret_steal<object>(detail::make_caster<T>::cast(
-                         std::forward<T>(x), return_value_policy::automatic, {}))),
+        : arg(std::move(base)), value(reinterpret_steal<object>(detail::make_caster<T>::cast(
+                                    std::forward<T>(x), return_value_policy::automatic, {}))),
           descr(descr)
 #if !defined(NDEBUG)
           ,
