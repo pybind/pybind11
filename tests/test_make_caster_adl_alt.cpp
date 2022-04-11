360eee89db7fdd938710b7b0e0202c14094fd81c
Laramie Leavitt <lar@google.com>
2022-04-11 13:54:56 -0700

    Update tests to use friend functions.
    Friend functions are preferable for ADL-based selection of
    the type casters.

--- tests/test_make_caster_adl_alt.cpp
+++ tests/test_make_caster_adl_alt.cpp
@@ -1,11 +1,13 @@
 #include "pybind11_tests.h"
 
 namespace have_a_ns {
-struct type_mock {};
 struct mock_caster_alt {
     static int num() { return 121; }
 };
-mock_caster_alt pybind11_select_caster(type_mock *);
+struct type_mock {
+  friend mock_caster_alt pybind11_select_caster(type_mock *);
+};
+
 } // namespace have_a_ns
 
 TEST_SUBMODULE(make_caster_adl_alt, m) {
