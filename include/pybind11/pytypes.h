25e4cc926c3b56726ad0a1d1a69ad4b0c589d6d6
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-05-17 22:31:30 -0700

    Bring back error_already_set copy ctor, to see if that resolves the 4 MSVC test failures.

--- include/pybind11/pytypes.h
+++ include/pybind11/pytypes.h
@@ -398,7 +398,7 @@ public:
         }
     }
 
-    error_already_set(const error_already_set &) = delete;
+    error_already_set(const error_already_set &) = default;
     error_already_set(error_already_set &&) = default;
 
     inline ~error_already_set() override;
