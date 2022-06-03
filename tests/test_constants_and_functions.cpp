a8269ba515b7ab246bafdb617ca42be747359b05
Aaron Gokaslan <skylion.aaron@gmail.com>
2022-06-03 12:44:00 -0400

    Use better loop var for char

--- tests/test_constants_and_functions.cpp
+++ tests/test_constants_and_functions.cpp
@@ -31,8 +31,8 @@ py::bytes return_bytes() {
 std::string print_bytes(const py::bytes &bytes) {
     std::string ret = "bytes[";
     const auto value = static_cast<std::string>(bytes);
-    for (char i : value) {
-        ret += std::to_string(static_cast<int>(i)) + ' ';
+    for (char c : value) {
+        ret += std::to_string(static_cast<int>(c)) + ' ';
     }
     ret.back() = ']';
     return ret;
