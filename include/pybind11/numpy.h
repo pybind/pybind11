92449586fb56cdd6f0906261f88d701a1471c5db
Aaron Gokaslan <skylion.aaron@gmail.com>
2022-06-06 14:01:58 -0400

    Revert to old dict api

--- include/pybind11/numpy.h
+++ include/pybind11/numpy.h
@@ -648,9 +648,10 @@ private:
         std::vector<field_descr> field_descriptors;
         field_descriptors.reserve(field_dict.size());
 
-        for (auto kv_item : field_dict) {
-            auto name = reinterpret_borrow<pybind11::str>(kv_item.first);
-            auto spec_fo = reinterpret_borrow<tuple>(kv_item.second);
+        for (auto field : field_dict.attr("items")()) {
+            auto spec = field.cast<tuple>();
+            auto name = spec[0].cast<pybind11::str>();
+            auto spec_fo = spec[1].cast<tuple>();
             auto format = spec_fo[0].cast<dtype>();
             auto offset = spec_fo[1].cast<pybind11::int_>();
             if ((len(name) == 0u) && format.kind() == 'V') {
