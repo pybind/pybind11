6fa2bb161e331c85ba4c6f79e73642e68dd7a008
Taiju Yamada <taiju.yamada@mujin.co.jp>
2022-06-15 13:17:11 +0900

    actually need both tests

--- tests/test_pytypes.cpp
+++ tests/test_pytypes.cpp
@@ -127,8 +127,12 @@ TEST_SUBMODULE(pytypes, m) {
     m.def("tuple_size_t", []() { return py::tuple{(py::size_t) 0}; });
     m.def("get_tuple", []() { return py::make_tuple(42, py::none(), "spam"); });
     m.def("access_tuple_with_int_index", []() {
-        py::object tpl = py::make_tuple(py::make_tuple(1, 2), py::make_tuple(3, 4));
-        return tpl[0][1];
+        py::object tpl = py::make_tuple(1, 2);
+        return tpl[1];
+    });
+    m.def("access_tuple_with_int_index_multidimension", []() {
+        py::object tpl = py::make_tuple(py::make_tuple(1, 2, 3), py::make_tuple(3, 4, 5));
+        return tpl[1][2];
     });
 
     // test_simple_namespace
