f5a11243b9d3a2db2ec2d85ce993ad9753b08eaa
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-06-21 02:40:40 -0700

    load_type fixes & follow-on cleanup

--- tests/test_odr_guard_2.py
+++ tests/test_odr_guard_2.py
@@ -2,8 +2,8 @@ import pybind11_tests.odr_guard_2 as m
 
 
 def test_type_mrc_to_python():
-    assert m.type_mrc_to_python() == 2222
+    assert m.type_mrc_to_python() in (202 + 2020, 202 + 1010)
 
 
 def test_type_mrc_from_python():
-    assert m.type_mrc_from_python("ignored") == 222
+    assert m.type_mrc_from_python("ignored") in (200 + 22, 200 + 11)
