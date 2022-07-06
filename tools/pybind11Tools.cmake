c0ef15a1771380f68126c74b58f62d8c57b51e01
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-07-05 19:45:50 -0700

    Force CXX_VISIBILITY_PRESET, CUDA_VISIBILITY_PRESET default

--- tools/pybind11Tools.cmake
+++ tools/pybind11Tools.cmake
@@ -189,11 +189,11 @@ function(pybind11_add_module target_name)
   # namespace; also turning it on for a pybind module compilation here avoids
   # potential warnings or issues from having mixed hidden/non-hidden types.
   if(NOT DEFINED CMAKE_CXX_VISIBILITY_PRESET)
-    set_target_properties(${target_name} PROPERTIES CXX_VISIBILITY_PRESET "hidden")
+    set_target_properties(${target_name} PROPERTIES CXX_VISIBILITY_PRESET "default")
   endif()
 
   if(NOT DEFINED CMAKE_CUDA_VISIBILITY_PRESET)
-    set_target_properties(${target_name} PROPERTIES CUDA_VISIBILITY_PRESET "hidden")
+    set_target_properties(${target_name} PROPERTIES CUDA_VISIBILITY_PRESET "default")
   endif()
 
   if(ARG_NO_EXTRAS)
