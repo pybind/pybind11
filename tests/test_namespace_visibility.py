8b28edd67dbd7835735d7cfe123280f8edb53dd6
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-07-08 14:20:35 -0700

    Try `import pybind11_cross_module_tests` from test_namespace_visibility.py

--- tests/test_namespace_visibility.py
+++ tests/test_namespace_visibility.py
@@ -1,9 +1,11 @@
 import namespace_visibility_1
 import namespace_visibility_2
+import pybind11_cross_module_tests as cm
 import pytest
 
 
 def test_namespace_visibility():
+    del cm  # Unused.
     modules = (
         namespace_visibility_1,
         namespace_visibility_2,
