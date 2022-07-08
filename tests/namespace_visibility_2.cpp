5ce9cdecdbea6613700adda472b3c6a8d2edefe3
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-07-08 12:11:03 -0700

    Stripping down even further.

--- tests/namespace_visibility_2.cpp
+++ tests/namespace_visibility_2.cpp
@@ -1,23 +1,3 @@
 #include "pybind11/pybind11.h"
-#include "namespace_visibility.h"
 
-#ifdef NEVER_DEFINED
-// clang-format off
-namespace pybind11_ns_vis_uuu PYBIND11_NS_VIS_U { PYBIND11_NS_VIS_FUNC }
-namespace pybind11_ns_vis_uuh PYBIND11_NS_VIS_H { PYBIND11_NS_VIS_FUNC }
-namespace pybind11_ns_vis_uhu PYBIND11_NS_VIS_U { PYBIND11_NS_VIS_FUNC }
-namespace pybind11_ns_vis_uhh PYBIND11_NS_VIS_H { PYBIND11_NS_VIS_FUNC }
-namespace pybind11_ns_vis_huu PYBIND11_NS_VIS_U { PYBIND11_NS_VIS_FUNC }
-namespace pybind11_ns_vis_huh PYBIND11_NS_VIS_H { PYBIND11_NS_VIS_FUNC }
-namespace pybind11_ns_vis_hhu PYBIND11_NS_VIS_U { PYBIND11_NS_VIS_FUNC }
-namespace pybind11_ns_vis_hhh PYBIND11_NS_VIS_H { PYBIND11_NS_VIS_FUNC }
-//                          ^                 ^
-//                   bit used ............ here
-// clang-format on
-#endif
-
-PYBIND11_MODULE(namespace_visibility_2, m) {
-    m.doc() = "DISABLED";
-
-    // PYBIND11_NS_VIS_DEFS
-}
+PYBIND11_MODULE(namespace_visibility_2, m) { m.doc() = "ns_vis_2"; }
--- tests/test_namespace_visibility.py
+++ tests/test_namespace_visibility.py
@@ -1,46 +1,13 @@
-# This is not really a unit test, but probing environment/toolchain/platform-specific
-# behavior under the exact same conditions as normal tests.
-# The results are useful to understanding the effects of, e.g., removing
-# `-fvisibility=hidden` or `__attribute__((visibility("hidden")))`, or linking
-# extensions statically with the core Python interpreter.
-
-import itertools
-
 import namespace_visibility_1
 import namespace_visibility_2
 import pytest
 
 
-@pytest.mark.skipif(
-    namespace_visibility_1.__doc__ == "DISABLED", reason="DISABLED for debugging"
-)
 def test_namespace_visibility():
     modules = (
         namespace_visibility_1,
-        namespace_visibility_1,  # .submodule,
         namespace_visibility_2,
     )
-    unique_pointer_labels = "ABC"
-    unique_pointers_observed = []
-    # u = visibility unspecified
-    # h = visibility hidden
-    for visibility in itertools.product(*([("u", "h")] * len(modules))):
-        # See functions in namespace_visibility_*.cpp
-        func = "ns_vis_" + "".join(visibility) + "_func"
-        ptrs = []
-        uq_ptrs_obs = ""
-        for vis, m in zip(visibility, modules):
-            ptr = getattr(m, func)()
-            ptrs.append(ptr)
-            lbl = unique_pointer_labels[ptrs.index(ptr)]
-            if vis == "h":
-                # Encode u/h info as upper/lower case to make the final result
-                # as compact as possible.
-                lbl = lbl.lower()
-            uq_ptrs_obs += lbl
-        unique_pointers_observed.append(uq_ptrs_obs)
-    all_unique_pointers_observed = ":".join(unique_pointers_observed)
-    if all_unique_pointers_observed != "AAC:AAc:AaC:Aac:aAC:aAc:aaC:aac":
-        pytest.skip(
-            f"UNUSUAL all_unique_pointers_observed: {all_unique_pointers_observed}"
-        )
+    for m in modules:
+        if m.__doc__ != "ns_vis_1":
+            pytest.skip(f"Not surprised: {m.__doc__}")
