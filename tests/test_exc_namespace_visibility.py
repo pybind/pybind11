dd4170d80213412d407a737a7c66989f0ac1c690
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-07-08 21:36:26 -0700

    Complete the documentation (comments in test_exc_namespace_visibility.py).

--- tests/test_exc_namespace_visibility.py
+++ tests/test_exc_namespace_visibility.py
@@ -4,15 +4,35 @@
 # `-fvisibility=hidden` or `__attribute__((visibility("hidden")))`, or linking
 # extensions statically with the core Python interpreter.
 
+# NOTE
+# ====
 # The "exc_" in "test_exc_namespace_visibility.py" is a workaround, to avoid a
-# test_cross_module_exception_translator (test_exceptions.py) failure.
-# See https://github.com/pybind/pybind11/pull/4054 for more information.
+# test_cross_module_exception_translator (test_exceptions.py) failure. This
+# test has to be imported (by pytest) before test_exceptions.py; pytest sorts
+# lexically. See https://github.com/pybind/pybind11/pull/4054 for more information.
 
 import itertools
 
 import namespace_visibility_1
 import namespace_visibility_2
 import pytest
+
+# Please take a quick look at namespace_visibility.h first, to see what is being probed.
+#
+# EXPECTED is for -fvisibility=hidden or equivalent, as recommended in the docs.
+EXPECTED_ALL_UNIQUE_POINTERS_OBSERVED = "AAC:AAc:AaC:Aac:aAC:aAc:aaC:aac"
+#                                        ^^^
+#          namespace_visibility_1 pointer|||
+# namespace_visibility_1.submodule pointer|| (identical letters means same pointer)
+#            namespace_visibility_2 pointer|
+# Upper-case: namespace visibility unspecified
+# Lower-case: namespace visibility hidden
+# This test probes all 2**3 combinations of u/h ** number-of-sub/modules.
+#
+# Also observed:
+# AAA:AAc:AaC:Aac:aAC:aAc:aaC:aac  -fvisibility=default Linux
+# AAA:AAc:AaA:Aac:aAC:aAc:aaC:aac  -fvisibility=default macOS
+# AAA:AAa:AaA:Aaa:aAA:aAa:aaA:aaa  everything linked statically
 
 
 def test_namespace_visibility():
@@ -41,7 +61,7 @@ def test_namespace_visibility():
             uq_ptrs_obs += lbl
         unique_pointers_observed.append(uq_ptrs_obs)
     all_unique_pointers_observed = ":".join(unique_pointers_observed)
-    if all_unique_pointers_observed != "AAC:AAc:AaC:Aac:aAC:aAc:aaC:aac":
+    if all_unique_pointers_observed != EXPECTED_ALL_UNIQUE_POINTERS_OBSERVED:
         pytest.skip(
             f"UNUSUAL all_unique_pointers_observed: {all_unique_pointers_observed}"
         )
