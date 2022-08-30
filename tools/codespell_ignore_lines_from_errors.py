09f49c25a27ab1e929914b66483becfba5af81c4
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-08-29 18:14:13 -0700

    Introduce .codespell-ignore-lines for safer (line-based instead of word-based) suppressions.

---
+++ tools/codespell_ignore_lines_from_errors.py
@@ -0,0 +1,31 @@
+"""Simple script for rebuilding .codespell-ignore-lines
+
+Usage:
+
+cat < /dev/null > .codespell-ignore-lines
+pre-commit run --all-files codespell >& /tmp/codespell_errors.txt
+python3 tools/codespell_ignore_lines_from_errors.py /tmp/codespell_errors.txt > .codespell-ignore-lines
+
+git diff to review changes, then commit, push.
+"""
+
+import sys
+from typing import List
+
+
+def run(args: List[str]) -> None:
+    assert len(args) == 1, "codespell_errors.txt"
+    cache = {}
+    for line in open(args[0]).read().splitlines():
+        i = line.find(" ==> ")
+        if i > 0:
+            flds = line[:i].split(":")
+            if len(flds) >= 2:
+                filename, line_num = flds[:2]
+                if filename not in cache:
+                    cache[filename] = open(filename).read().splitlines()
+                print(cache[filename][int(line_num) - 1])
+
+
+if __name__ == "__main__":
+    run(args=sys.argv[1:])
