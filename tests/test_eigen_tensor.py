51bdba8bbf4ed39a34e6cdf55cfc6adcd4679312
Ethan Steinberg <ethanid@stanford.edu>
2022-10-07 20:28:18 +0000

    Add test to make sure unsafe casts fail

--- tests/test_eigen_tensor.py
+++ tests/test_eigen_tensor.py
@@ -142,6 +142,12 @@ def test_references_actually_refer(m):
 @pytest.mark.parametrize("m", submodules)
 def test_round_trip(m):
     assert_equal_tensor_ref(m.round_trip_tensor(tensor_ref))
+
+    with pytest.raises(TypeError):
+        assert_equal_tensor_ref(m.round_trip_tensor2(tensor_ref))
+
+    assert_equal_tensor_ref(m.round_trip_tensor2(np.array(tensor_ref, dtype=np.int32)))
+
     assert_equal_tensor_ref(m.round_trip_fixed_tensor(tensor_ref))
     assert_equal_tensor_ref(m.round_trip_aligned_view_tensor(m.reference_tensor()))
 
