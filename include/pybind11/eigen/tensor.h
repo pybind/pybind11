fff0c96edd541381bfcf07e6b25df17aaadbd65c
Ethan Steinberg <ethanid@stanford.edu>
2022-10-11 18:56:23 +0000

    Make clang happy

--- include/pybind11/eigen/tensor.h
+++ include/pybind11/eigen/tensor.h
@@ -137,12 +137,16 @@ struct get_tensor_descriptor {
 
 template <typename T, int size>
 std::vector<T> convert_dsizes_to_vector(const Eigen::DSizes<T, size> &arr) {
-    std::vector<T> result(size);
-    for (int i = 0; i < size; i++) {
-        result[i] = arr[i];
-    }
+    if (size == 0) {
+        return {};
+    } else {
+        std::vector<T> result(size);
+        for (size_t i = 0; i < size; i++) {
+            result[i] = arr[i];
+        }
 
-    return result;
+        return result;
+    }
 }
 
 template <typename Type>
