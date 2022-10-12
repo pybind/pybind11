129c6cf7d1ae0add60062367852febdf46ce4a2d
Aaron Gokaslan <aaronGokaslan@gmail.com>
2022-10-12 15:18:48 -0700

    Remove redundant exc checking

--- include/pybind11/pytypes.h
+++ include/pybind11/pytypes.h
@@ -2357,11 +2357,7 @@ args_proxy object_api<D>::operator*() const {
 template <typename D>
 template <typename T>
 bool object_api<D>::contains(T &&item) const {
-    auto ret = attr("__contains__")(std::forward<T>(item)).template cast<bool>();
-    if (PyErr_Occurred()) {
-        throw error_already_set();
-    }
-    return ret;
+    return attr("__contains__")(std::forward<T>(item)).template cast<bool>();
 }
 
 template <typename D>
