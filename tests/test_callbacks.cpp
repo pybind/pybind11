4c6c768898f70e2b1e2c9924b05b074d6dc2962d
Aaron Gokaslan <aaronGokaslan@gmail.com>
2022-10-20 09:53:14 -0400

    Do not make new CTOR just yet

--- tests/test_callbacks.cpp
+++ tests/test_callbacks.cpp
@@ -253,8 +253,8 @@ TEST_SUBMODULE(callbacks, m) {
 
     // rec_capsule with custom name
     constexpr const char *rec_capsule_name = "CUSTOM_REC_CAPSULE";
-    py::capsule rec_capsule(
-        malloc(1), [](void *data) { free(data); }, rec_capsule_name);
+    py::capsule rec_capsule(malloc(1), [](void *data) { free(data); });
+    rec_capsule.set_name(rec_capsule_name);
     py::handle m_ptr = PyCFunction_New(&def, rec_capsule.ptr());
     m.add_object("custom_function", m_ptr);
 }
