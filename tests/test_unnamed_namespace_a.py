191267055bb9904e2b948a60736225e87af342b2
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-11-07 04:13:28 -0800

    test_unnamed_namespace_b xfail for clang

--- tests/test_unnamed_namespace_a.py
+++ tests/test_unnamed_namespace_a.py
@@ -1,5 +1,8 @@
+# NOTE: This test relies on pytest SORT ORDER:
+#       test_unnamed_namespace_a.py imported before test_unnamed_namespace_b.py
+
 from pybind11_tests import unnamed_namespace_a as m
 
 
-def test_have_type():
-    assert hasattr(m, "unnamed_namespace_a_any_struct")
+def test_have_class_any_struct():
+    assert m.unnamed_namespace_a_any_struct is not None
--- tests/test_unnamed_namespace_b.py
+++ tests/test_unnamed_namespace_b.py
@@ -1,5 +1,15 @@
+# NOTE: This test relies on pytest SORT ORDER:
+#       test_unnamed_namespace_a.py imported before test_unnamed_namespace_b.py
+
+import pytest
+
 from pybind11_tests import unnamed_namespace_b as m
 
 
-def test_have_type():
-    assert hasattr(m, "unnamed_namespace_b_any_struct")
+@pytest.mark.xfail(
+    "m.defined___clang__",
+    reason="Known issue with all clang versions: https://github.com/pybind/pybind11/pull/4316",
+    strict=True,
+)
+def test_have_class_any_struct():
+    assert m.unnamed_namespace_b_any_struct is not None
