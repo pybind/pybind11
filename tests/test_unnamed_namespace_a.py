6f1ddb246f803f85fd26203656c811930c1ae97e
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-11-09 20:50:49 -0800

    Adjust expectations for Windows Clang (and make code less redundant).

--- tests/test_unnamed_namespace_a.py
+++ tests/test_unnamed_namespace_a.py
@@ -3,14 +3,16 @@ import pytest
 from pybind11_tests import unnamed_namespace_a as m
 from pybind11_tests import unnamed_namespace_b as mb
 
+XFAIL_CONDITION = "not m.defined_WIN32_or__WIN32 and (m.defined___clang__ or m.defined__LIBCPP_VERSION)"
+XFAIL_REASON = "Known issues: https://github.com/pybind/pybind11/pull/4319"
 
-@pytest.mark.xfail(
-    "m.defined___clang__ or m.defined__LIBCPP_VERSION",
-    reason="Known issues: https://github.com/pybind/pybind11/pull/4319",
-    strict=False,
+
+@pytest.mark.xfail(XFAIL_CONDITION, reason=XFAIL_REASON, strict=False)
+@pytest.mark.parametrize(
+    "any_struct", (m.unnamed_namespace_a_any_struct, mb.unnamed_namespace_b_any_struct)
 )
-def test_have_class_any_struct():
-    assert m.unnamed_namespace_a_any_struct is not None
+def test_have_class_any_struct(any_struct):
+    assert any_struct is not None
 
 
 def test_have_at_least_one_class_any_struct():
@@ -20,11 +22,7 @@ def test_have_at_least_one_class_any_struct():
     )
 
 
-@pytest.mark.xfail(
-    "m.defined___clang__ or m.defined__LIBCPP_VERSION",
-    reason="Known issues: https://github.com/pybind/pybind11/pull/4319",
-    strict=True,
-)
+@pytest.mark.xfail(XFAIL_CONDITION, reason=XFAIL_REASON, strict=True)
 def test_have_both_class_any_struct():
     assert (
         m.unnamed_namespace_a_any_struct is not None
