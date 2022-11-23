74bbc04912bdea4f0930ccaaddeb7d3d531a79a0
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-11-22 16:18:08 -0800

    Add "In a nutshell" comment, in response to request by @EricCousineau-TRI

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,13 @@ import pytest
 import pybind11_tests
 
 if os.name != "nt":
-    # https://github.com/pybind/pybind11/issues/4105#issuecomment-1301004592
+    # Full background: https://github.com/pybind/pybind11/issues/4105#issuecomment-1301004592
+    # In a nutshell: fork() after starting threads == flakiness in the form of deadlocks.
+    # It is actually a well-known pitfall, unfortunately without guard rails.
+    # "forkserver" is more performant than "spawn" (~9s vs ~13s for tests/test_gil_scoped.py,
+    # visit the issuecomment link above for details).
+    # Windows does not have fork() and the associated pitfall, therefore it is best left
+    # running with defaults.
     multiprocessing.set_start_method("forkserver")
 
 _long_marker = re.compile(r"([0-9])L")
