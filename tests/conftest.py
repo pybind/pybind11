2c7afbcd823e0c1c30091dc17bd1363e41a7bc58
Aaron Gokaslan <aaronGokaslan@gmail.com>
2022-12-01 09:51:48 -0800

    chore: convert multiprocessing set_spawn to fixture in pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,15 +17,19 @@ import pytest
 # Early diagnostic for failed imports
 import pybind11_tests
 
-if os.name != "nt":
-    # Full background: https://github.com/pybind/pybind11/issues/4105#issuecomment-1301004592
-    # In a nutshell: fork() after starting threads == flakiness in the form of deadlocks.
-    # It is actually a well-known pitfall, unfortunately without guard rails.
-    # "forkserver" is more performant than "spawn" (~9s vs ~13s for tests/test_gil_scoped.py,
-    # visit the issuecomment link above for details).
-    # Windows does not have fork() and the associated pitfall, therefore it is best left
-    # running with defaults.
-    multiprocessing.set_start_method("forkserver")
+
+@pytest.fixture(scope="session", autouse=True)
+def always_forkserver_on_unix():
+    if os.name != "nt":
+        # Full background: https://github.com/pybind/pybind11/issues/4105#issuecomment-1301004592
+        # In a nutshell: fork() after starting threads == flakiness in the form of deadlocks.
+        # It is actually a well-known pitfall, unfortunately without guard rails.
+        # "forkserver" is more performant than "spawn" (~9s vs ~13s for tests/test_gil_scoped.py,
+        # visit the issuecomment link above for details).
+        # Windows does not have fork() and the associated pitfall, therefore it is best left
+        # running with defaults.
+        multiprocessing.set_start_method("forkserver")
+
 
 _long_marker = re.compile(r"([0-9])L")
 _hexadecimal = re.compile(r"0x[0-9a-fA-F]+")
