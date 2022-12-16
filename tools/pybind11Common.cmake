a9f3e9d6115f4063478f092e677f935891f02ee2
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2022-12-15 20:25:55 -0800

    Add WARNING/HELP WANTED comment.

--- tools/pybind11Common.cmake
+++ tools/pybind11Common.cmake
@@ -313,6 +313,7 @@ function(_pybind11_generate_lto target prefer_thin_lto)
     endif()
   elseif(CMAKE_CXX_COMPILER_ID MATCHES "IntelLLVM")
     # IntelLLVM equivalent to LTO is called IPO; also IntelLLVM is WIN32/UNIX
+    # WARNING/HELP WANTED: This block of code is currently not covered by pybind11 GitHub Actions!
     if(WIN32)
       _pybind11_return_if_cxx_and_linker_flags_work(
         HAS_INTEL_IPO "-Qipo" "-Qipo" PYBIND11_LTO_CXX_FLAGS PYBIND11_LTO_LINKER_FLAGS)
