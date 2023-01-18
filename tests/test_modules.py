f87da1bf48e937c76f941f23882cb09168642c42
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2023-01-18 13:18:33 -0800

    Tweak comment instead of removing it.

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -107,6 +107,9 @@ def test_def_submodule_failures():
         sm_name_orig = sm.__name__
         sm.__name__ = malformed_utf8
         try:
+            # We want to assert that a bad __name__ causes some kind of failure, although we do not want to exercise
+            # the internals of PyModule_GetName(). Currently all supported Python versions raise SystemError. If that
+            # changes in future Python versions, simply add the new expected exception types here.
             with pytest.raises(SystemError):
                 m.def_submodule(sm, b"SubSubModuleName")
         finally:
