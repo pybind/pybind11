5b0a6fc2017fcc176545afe3e09c9f9885283242
Henry Schreiner <henryschreineriii@gmail.com>
2023-03-16 17:03:10 -0400

    chore: bump version to 3.10.4
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -11,11 +11,11 @@
 
 #define PYBIND11_VERSION_MAJOR 2
 #define PYBIND11_VERSION_MINOR 10
-#define PYBIND11_VERSION_PATCH 3
+#define PYBIND11_VERSION_PATCH 4
 
 // Similar to Python's convention: https://docs.python.org/3/c-api/apiabiversion.html
 // Additional convention: 0xD = dev
-#define PYBIND11_VERSION_HEX 0x020A0300
+#define PYBIND11_VERSION_HEX 0x020A0400
 
 // Define some generic pybind11 helper macros for warning management.
 //
