432de631857a5659c8e59e717f185e6988841313
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2023-04-01 12:08:39 -0700

    Add missing test, fix bug (missing `throw error_already_set();`), various cosmetic changes.

--- tests/test_type_caster_pyobject_ptr.cpp
+++ tests/test_type_caster_pyobject_ptr.cpp
@@ -28,6 +28,7 @@ public:
         }
         if (PyErr_Occurred()) {
             raise_from(PyExc_SystemError, "src != nullptr but PyErr_Occurred()");
+            throw error_already_set();
         }
         if (policy == return_value_policy::take_ownership) {
             return src;
@@ -54,38 +55,43 @@ private:
 } // namespace pybind11
 
 TEST_SUBMODULE(type_caster_pyobject_ptr, m) {
-    m.def("cast_from_PyObject_ptr", []() {
+    m.def("cast_from_pyobject_ptr", []() {
         PyObject *ptr = PyLong_FromLongLong(6758L);
         py::object retval = py::cast(ptr, py::return_value_policy::take_ownership);
         return retval;
     });
-    m.def("cast_to_PyObject_ptr", [](py::handle obj) {
+    m.def("cast_to_pyobject_ptr", [](py::handle obj) {
         auto *ptr = py::cast<PyObject *>(obj);
         return bool(PyTuple_CheckExact(ptr));
     });
 
     m.def(
-        "return_PyObject_ptr",
+        "return_pyobject_ptr",
         []() { return PyLong_FromLongLong(2314L); },
         py::return_value_policy::take_ownership);
-    m.def("pass_PyObject_ptr", [](PyObject *obj) { return bool(PyTuple_CheckExact(obj)); });
+    m.def("pass_pyobject_ptr", [](PyObject *obj) { return bool(PyTuple_CheckExact(obj)); });
 
     m.def("call_callback_with_object_return",
           [](const std::function<py::object(int mode)> &cb, int mode) { return cb(mode); });
     m.def("call_callback_with_handle_return",
           [](const std::function<py::handle(int mode)> &cb, int mode) { return cb(mode); });
     //
-    m.def("call_callback_with_PyObject_ptr_return",
+    m.def("call_callback_with_pyobject_ptr_return",
           [](const std::function<PyObject *(int mode)> &cb, int mode) { return cb(mode); });
-    m.def("call_callback_with_PyObject_ptr_arg",
+    m.def("call_callback_with_pyobject_ptr_arg",
           [](const std::function<bool(PyObject *)> &cb, py::handle obj) { return cb(obj.ptr()); });
 
-    m.def("cast_nullptr", [](bool set_error) {
+    m.def("cast_to_pyobject_ptr_nullptr", [](bool set_error) {
         if (set_error) {
-            PyErr_SetString(PyExc_RuntimeError, "As in functioning error handling.");
+            PyErr_SetString(PyExc_RuntimeError, "Reflective of healthy error handling.");
         }
         PyObject *ptr = nullptr;
         py::cast(ptr);
+    });
+
+    m.def("cast_to_pyobject_ptr_non_nullptr_with_error_set", []() {
+        PyErr_SetString(PyExc_RuntimeError, "Reflective of unhealthy error handling.");
+        py::cast(Py_None);
     });
 
 #ifdef PYBIND11_NO_COMPILE_SECTION // Change to ifndef for manual testing.
--- tests/test_type_caster_pyobject_ptr.py
+++ tests/test_type_caster_pyobject_ptr.py
@@ -3,22 +3,22 @@ import pytest
 from pybind11_tests import type_caster_pyobject_ptr as m
 
 
-def test_cast_from_PyObject_ptr():
-    assert m.cast_from_PyObject_ptr() == 6758
+def test_cast_from_pyobject_ptr():
+    assert m.cast_from_pyobject_ptr() == 6758
 
 
-def test_cast_to_PyObject_ptr():
-    assert m.cast_to_PyObject_ptr(())
-    assert not m.cast_to_PyObject_ptr({})
+def test_cast_to_pyobject_ptr():
+    assert m.cast_to_pyobject_ptr(())
+    assert not m.cast_to_pyobject_ptr({})
 
 
-def test_return_PyObject_ptr():
-    assert m.return_PyObject_ptr() == 2314
+def test_return_pyobject_ptr():
+    assert m.return_pyobject_ptr() == 2314
 
 
-def test_pass_PyObject_ptr():
-    assert m.pass_PyObject_ptr(())
-    assert not m.pass_PyObject_ptr({})
+def test_pass_pyobject_ptr():
+    assert m.pass_pyobject_ptr(())
+    assert not m.pass_pyobject_ptr({})
 
 
 @pytest.mark.parametrize(
@@ -26,7 +26,7 @@ def test_pass_PyObject_ptr():
     [
         m.call_callback_with_object_return,
         m.call_callback_with_handle_return,
-        m.call_callback_with_PyObject_ptr_return,
+        m.call_callback_with_pyobject_ptr_return,
     ],
 )
 def test_call_callback_with_object_return(call_callback):
@@ -43,22 +43,29 @@ def test_call_callback_with_object_return(call_callback):
         call_callback(cb, 2)
 
 
-def test_call_callback_with_PyObject_ptr_arg():
+def test_call_callback_with_pyobject_ptr_arg():
     def cb(obj):
         return isinstance(obj, tuple)
 
-    assert m.call_callback_with_PyObject_ptr_arg(cb, ())
-    assert not m.call_callback_with_PyObject_ptr_arg(cb, {})
+    assert m.call_callback_with_pyobject_ptr_arg(cb, ())
+    assert not m.call_callback_with_pyobject_ptr_arg(cb, {})
 
 
 @pytest.mark.parametrize("set_error", [True, False])
-def test_cast_nullptr(set_error):
+def test_cast_to_python_nullptr(set_error):
     expected = {
-        True: r"As in functioning error handling\.",
+        True: r"^Reflective of healthy error handling\.$",
         False: (
-            r"Internal error: pybind11::error_already_set called "
-            r"while Python error indicator not set\."
+            r"^Internal error: pybind11::error_already_set called "
+            r"while Python error indicator not set\.$"
         ),
     }[set_error]
     with pytest.raises(RuntimeError, match=expected):
-        m.cast_nullptr(set_error)
+        m.cast_to_pyobject_ptr_nullptr(set_error)
+
+
+def test_cast_to_python_non_nullptr_with_error_set():
+    with pytest.raises(SystemError) as excinfo:
+        m.cast_to_pyobject_ptr_non_nullptr_with_error_set()
+    assert str(excinfo.value) == "src != nullptr but PyErr_Occurred()"
+    assert str(excinfo.value.__cause__) == "Reflective of unhealthy error handling."
