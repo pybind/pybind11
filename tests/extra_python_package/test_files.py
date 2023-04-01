b3930018599c986bf17a037e1a628d8943a833b6
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2023-04-01 12:55:42 -0700

    Add new header file to CMakeLists.txt and tests/extra_python_package/test_files.py

--- tests/extra_python_package/test_files.py
+++ tests/extra_python_package/test_files.py
@@ -43,6 +43,7 @@ main_headers = {
     "include/pybind11/pytypes.h",
     "include/pybind11/stl.h",
     "include/pybind11/stl_bind.h",
+    "include/pybind11/type_caster_pyobject_ptr.h",
 }
 
 detail_headers = {
