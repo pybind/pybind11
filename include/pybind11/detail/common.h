db98bd889146fa5e34f31ccc97120c88721720d2
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2023-05-22 13:26:12 -0700

    [ci skip] Revert "Remove .dev1 from version number."
    This reverts commit afc80134cb1035be2913fd8153a16a864181c52c.

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -11,11 +11,11 @@
 
 #define PYBIND11_VERSION_MAJOR 2
 #define PYBIND11_VERSION_MINOR 11
-#define PYBIND11_VERSION_PATCH 0
+#define PYBIND11_VERSION_PATCH 0.dev1
 
 // Similar to Python's convention: https://docs.python.org/3/c-api/apiabiversion.html
 // Additional convention: 0xD = dev
-#define PYBIND11_VERSION_HEX 0x020B0000
+#define PYBIND11_VERSION_HEX 0x020B00D1
 
 // Define some generic pybind11 helper macros for warning management.
 //
