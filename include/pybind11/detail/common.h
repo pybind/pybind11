f67e2acefb1aad16638cded6f23e87407947bf8e
Ethan Steinberg <ethan.steinberg@gmail.com>
2023-07-16 08:30:44 -0700

    Update common.h

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -324,7 +324,7 @@ PYBIND11_WARNING_POP
 #endif
 
 // See description of PR #4246:
-#if !defined(PYBIND11_NO_GIL_CHECKS) && !defined(NDEBUG)                                          \
+#if !defined(PYBIND11_NO_ASSERT_GIL_HELD_INCREF_DECREF) && !defined(NDEBUG)                                          \
     && !defined(PY_ASSERT_GIL_HELD_INCREF_DECREF) && !defined(PYPY_VERSION)                       \
     && !defined(PYBIND11_ASSERT_GIL_HELD_INCREF_DECREF)
 #    define PYBIND11_ASSERT_GIL_HELD_INCREF_DECREF
