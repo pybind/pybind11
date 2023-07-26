bdd938ad7e0e52eb749e3e58555328f6f595d815
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2023-07-25 21:00:38 -0700

    Resolve clang-tidy errors.

--- tests/test_python_multiple_inheritance.cpp
+++ tests/test_python_multiple_inheritance.cpp
@@ -6,7 +6,7 @@ namespace test_python_multiple_inheritance {
 // https://github.com/google/clif/blob/5718e4d0807fd3b6a8187dde140069120b81ecef/clif/testing/python_multiple_inheritance.h
 
 struct CppBase {
-    CppBase(int value) : base_value(value) {}
+    explicit CppBase(int value) : base_value(value) {}
     int get_base_value() const { return base_value; }
     void reset_base_value(int new_value) { base_value = new_value; }
 
@@ -15,7 +15,7 @@ private:
 };
 
 struct CppDrvd : CppBase {
-    CppDrvd(int value) : CppBase(value), drvd_value(value * 3) {}
+    explicit CppDrvd(int value) : CppBase(value), drvd_value(value * 3) {}
     int get_drvd_value() const { return drvd_value; }
     void reset_drvd_value(int new_value) { drvd_value = new_value; }
 
