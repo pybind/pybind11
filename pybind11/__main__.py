e062062136046fe5e4485b31b816baff42ecb57c
Markus Bauer <markus.bauer@cispa.saarland>
2023-10-05 16:34:33 +0200

    fix: Escape paths with spaces in include list from --includes

--- pybind11/__main__.py
+++ pybind11/__main__.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-function-docstring
 
 import argparse
+import shlex
 import sys
 import sysconfig
 
@@ -21,7 +22,7 @@ def print_includes() -> None:
         if d and d not in unique_dirs:
             unique_dirs.append(d)
 
-    print(" ".join("-I" + d for d in unique_dirs))
+    print(" ".join(shlex.quote("-I" + d) for d in unique_dirs))
 
 
 def main() -> None:
