398a42c0e8decdd4fdaee56fe6c08c46876814d7
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2023-10-09 12:35:23 -0700

    Fix oversight: update tests/extra_python_package/test_files.py

--- tests/extra_python_package/test_files.py
+++ tests/extra_python_package/test_files.py
@@ -35,6 +35,7 @@ main_headers = {
     "include/pybind11/eval.h",
     "include/pybind11/functional.h",
     "include/pybind11/gil.h",
+    "include/pybind11/gil_save_static_initialization.h",
     "include/pybind11/iostream.h",
     "include/pybind11/numpy.h",
     "include/pybind11/operators.h",
