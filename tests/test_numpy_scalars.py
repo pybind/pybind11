af4200ce4597df088fa8bb8b33713fa8f5d18738
sun1638650145 <1638650145@qq.com>
2024-03-29 12:16:17 +0800

    Bugfix.

--- tests/test_numpy_scalars.py
+++ tests/test_numpy_scalars.py
@@ -40,7 +40,7 @@ def scalar_type(request):
 def expected_signature(tp):
     s = "str" if sys.version_info[0] >= 3 else "unicode"
     t = type_name(tp)
-    return f"test_{t}(x: {t}) -> Tuple[{s}, {t}]\n"
+    return f"test_{t}(x: {t}) -> tuple[{s}, {t}]\n"
 
 
 def test_numpy_scalars(scalar_type):
