7a945d8f40cf946538ca2170a7adcf6dcd1e2509
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-06-10 19:05:21 -0700

    Revert "cast.h return_value_policy_override _clif_automatic (#4364)"
    This reverts commit 8720cf94d69a3fce302c2e2f7abed464a9e485c6.

--- include/pybind11/cast.h
+++ include/pybind11/cast.h
@@ -1170,7 +1170,6 @@ struct return_value_policy_override<
                         void>> {
     static return_value_policy policy(return_value_policy p) {
         return !std::is_lvalue_reference<Return>::value && !std::is_pointer<Return>::value
-                       && p != return_value_policy::_clif_automatic
                    ? return_value_policy::move
                    : p;
     }
