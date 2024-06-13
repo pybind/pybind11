c46afa47833b70c8c92a222917299a9dd2848d37
Michael Carlstrom <rmc@carlstrom.com>
2024-06-13 19:40:26 -0400

    add std::move for older code

--- tests/test_pytypes.cpp
+++ tests/test_pytypes.cpp
@@ -858,12 +858,12 @@ TEST_SUBMODULE(pytypes, m) {
 
     m.def("union_typing_only",
           [](py::typing::List<py::typing::Union<py::str>> l)
-              -> py::typing::List<py::typing::Union<py::int_>> { return l; });
+              -> py::typing::List<py::typing::Union<py::int_>> { return std::move(l); });
 
     m.def("annotate_optional", []() -> py::typing::List<py::typing::Optional<py::str>> {
         auto list = py::list();
         list.append(py::str("hi"));
         list.append(py::none());
-        return list;
+        return std::move(list);
     });
 }
