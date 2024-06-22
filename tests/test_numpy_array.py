97ec7620fb4d7e5f6be00264f10e84b710888888
Henry Schreiner <HenrySchreinerIII@gmail.com>
2024-06-22 00:20:08 -0400

    Update test_numpy_array.py

--- tests/test_numpy_array.py
+++ tests/test_numpy_array.py
@@ -201,8 +201,6 @@ def test_wrap():
         assert a.flags.writeable == b.flags.writeable
         assert a.flags.aligned == b.flags.aligned
         assert a.flags.writebackifcopy == b.flags.writebackifcopy
-        else:
-            assert a.flags.updateifcopy == b.flags.updateifcopy
         assert np.all(a == b)
         assert not b.flags.owndata
         assert b.base is base
