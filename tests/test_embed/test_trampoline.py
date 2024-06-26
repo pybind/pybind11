c9a83e5216f038b166edce1ecce30acb048d33fb
Henry Schreiner <henryschreineriii@gmail.com>
2024-06-26 16:15:21 -0400

    tests: revert changes to test_embed
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_embed/test_trampoline.py
+++ tests/test_embed/test_trampoline.py
@@ -1,14 +1,5 @@
 from __future__ import annotations
 
-import sys
-
-import pytest
-
-if sys.platform.startswith("emscripten"):
-    pytest.skip(
-        "Test not implemented from single wheel on Pyodide", allow_module_level=True
-    )
-
 import trampoline_module
 
 
