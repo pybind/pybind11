0fe84efab108ef6591428c6633ac0a287149b0f1
Michael Carlstrom <rmc@carlstrom.com>
2024-06-27 18:50:19 -0400

    surely this time

--- include/pybind11/typing.h
+++ include/pybind11/typing.h
@@ -100,8 +100,7 @@ class Never : public none {
 
 // Define guard around specific GCC version
 // https://github.com/pybind/pybind11/issues/5201
-#if (defined(__cpp_nontype_template_parameter_class) && !defined(__GNUC__))                       \
-    || defined(__GNUC__) && (__GNUC__ > 10 || (__GNUC__ == 10 && __GNUC_MINOR__ >= 3))
+#if defined(__cpp_nontype_template_parameter_class) && (!defined(__GNUC__) || defined(__GNUC__) && (__GNUC__ > 10 || (__GNUC__ == 10 && __GNUC_MINOR__ >= 3)))
 template <size_t N>
 struct StringLiteral {
     constexpr StringLiteral(const char (&str)[N]) { std::copy_n(str, N, name); }
@@ -225,8 +224,7 @@ struct handle_type_name<typing::Never> {
     static constexpr auto name = const_name("Never");
 };
 
-#if (defined(__cpp_nontype_template_parameter_class) && !defined(__GNUC__))                       \
-    || defined(__GNUC__) && (__GNUC__ > 10 || (__GNUC__ == 10 && __GNUC_MINOR__ >= 3))
+#if defined(__cpp_nontype_template_parameter_class) && (!defined(__GNUC__) || defined(__GNUC__) && (__GNUC__ > 10 || (__GNUC__ == 10 && __GNUC_MINOR__ >= 3)))
 template <typing::StringLiteral... Literals>
 struct handle_type_name<typing::Literal<Literals...>> {
     static constexpr auto name = const_name("Literal[")
