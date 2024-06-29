e5907480f58b4d2bde36813d10ae7ff7d2d2cbfb
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-06-29 11:09:03 -0700

    Add `py::class_<SomeType>` with default ctor.

--- tests/test_wip.cpp
+++ tests/test_wip.cpp
@@ -1,3 +1,17 @@
 #include "pybind11_tests.h"
 
-TEST_SUBMODULE(wip, m) { m.attr("__doc__") = "WIP"; }
+namespace pybind11_tests {
+namespace wip {
+
+struct SomeType {};
+
+} // namespace wip
+} // namespace pybind11_tests
+
+TEST_SUBMODULE(wip, m) {
+    m.attr("__doc__") = "WIP";
+
+    using namespace pybind11_tests::wip;
+
+    py::class_<SomeType>(m, "SomeType").def(py::init<>());
+}
--- tests/test_wip.py
+++ tests/test_wip.py
@@ -5,3 +5,8 @@ from pybind11_tests import wip as m
 
 def test_doc():
     assert m.__doc__ == "WIP"
+
+
+def test_some_type_ctor():
+    obj = m.SomeType()
+    assert isinstance(obj, m.SomeType)
