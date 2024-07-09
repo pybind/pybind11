c5278145a04617e3409ff1d4c7464d7ec8a4165a
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-07-09 13:34:27 -0700

    Transfer test_mixed from test_class_sh_disowning to test_wip (preparation for debugging behavior difference).

--- tests/test_wip.cpp
+++ tests/test_wip.cpp
@@ -1,21 +1,32 @@
+#include <pybind11/smart_holder.h>
+
 #include "pybind11_tests.h"
 
 namespace pybind11_tests {
 namespace wip {
 
-struct SomeType {};
+template <int SerNo> // Using int as a trick to easily generate a series of types.
+struct Atype {
+    int val = 0;
+    explicit Atype(int val_) : val{val_} {}
+    int get() const { return val * 10 + SerNo; }
+};
+
+int mixed(std::unique_ptr<Atype<1>> at1, std::unique_ptr<Atype<2>> at2) {
+    return at1->get() * 200 + at2->get() * 20;
+}
 
 } // namespace wip
 } // namespace pybind11_tests
 
+using namespace pybind11_tests::wip;
+
+PYBIND11_SMART_HOLDER_TYPE_CASTERS(Atype<1>)
+PYBIND11_SMART_HOLDER_TYPE_CASTERS(Atype<2>)
+
 TEST_SUBMODULE(wip, m) {
-    m.attr("__doc__") = "WIP";
+    py::classh<Atype<1>>(m, "Atype1").def(py::init<int>()).def("get", &Atype<1>::get);
+    py::classh<Atype<2>>(m, "Atype2").def(py::init<int>()).def("get", &Atype<2>::get);
 
-    using namespace pybind11_tests::wip;
-
-    py::class_<SomeType>(m, "SomeType").def(py::init([]() {
-        return std::unique_ptr<SomeType>(new SomeType);
-    }));
-
-    m.def("make_some_type", []() { return std::unique_ptr<SomeType>(new SomeType); });
+    m.def("mixed", mixed);
 }
--- tests/test_wip.py
+++ tests/test_wip.py
@@ -1,17 +1,26 @@
 from __future__ import annotations
 
+import pytest
+
 from pybind11_tests import wip as m
 
 
-def test_doc():
-    assert m.__doc__ == "WIP"
+def test_mixed():
+    obj1a = m.Atype1(90)
+    obj2a = m.Atype2(25)
+    obj1b = m.Atype1(0)
+    obj2b = m.Atype2(0)
 
+    print("\nLOOOK A BEFORE m.mixed(obj1a, obj2a)", flush=True)
+    assert m.mixed(obj1a, obj2a) == (90 * 10 + 1) * 200 + (25 * 10 + 2) * 20
+    print("\nLOOOK A  AFTER m.mixed(obj1a, obj2a)", flush=True)
 
-def test_some_type_ctor():
-    obj = m.SomeType()
-    assert isinstance(obj, m.SomeType)
+    print("\nLOOOK B BEFORE m.mixed(obj1b, obj2a)", flush=True)
+    with pytest.raises(ValueError):
+        m.mixed(obj1b, obj2a)
+    print("\nLOOOK B  AFTER m.mixed(obj1b, obj2a)", flush=True)
 
-
-def test_make_some_type():
-    obj = m.make_some_type()
-    assert isinstance(obj, m.SomeType)
+    print("\nLOOOK C BEFORE m.mixed(obj1a, obj2b)", flush=True)
+    with pytest.raises(ValueError):
+        m.mixed(obj1a, obj2b)
+    print("\nLOOOK C  AFTER m.mixed(obj1a, obj2b)", flush=True)
