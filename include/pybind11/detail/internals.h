18b72c0ffa6ff2747ed6c4b869a80adfb8e762c9
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-07-20 19:57:52 -0700

    Tie `PYBIND11_INTERNALS_VERSION 6` to `PYBIND11_VERSION_MAJOR >= 3`

--- include/pybind11/detail/internals.h
+++ include/pybind11/detail/internals.h
@@ -36,12 +36,14 @@
 /// further ABI-incompatible changes may be made before the ABI is officially
 /// changed to the new version.
 #ifndef PYBIND11_INTERNALS_VERSION
-#    if PY_VERSION_HEX >= 0x030C0000 || defined(_MSC_VER)
+#    if PYBIND11_VERSION_MAJOR >= 3
+#        define PYBIND11_INTERNALS_VERSION 6
+#    elif PY_VERSION_HEX >= 0x030C0000 || defined(_MSC_VER)
 // Version bump for Python 3.12+, before first 3.12 beta release.
 // Version bump for MSVC piggy-backed on PR #4779. See comments there.
-#        define PYBIND11_INTERNALS_VERSION 6 // BAKEIN_WIP: Only do this for pybind11 v3.0.0
+#        define PYBIND11_INTERNALS_VERSION 5
 #    else
-#        define PYBIND11_INTERNALS_VERSION 6 // BAKEIN_WIP: Only do this for pybind11 v3.0.0
+#        define PYBIND11_INTERNALS_VERSION 4
 #    endif
 #endif
 
