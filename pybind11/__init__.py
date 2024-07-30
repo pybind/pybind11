49580cfa977eaef89b95d070e59214ebc9a05c43
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-07-29 19:51:53 -0700

    Change `[tool.ruff]` `target-version` to `"py38"`, as suggested by @Skylion007

--- pybind11/__init__.py
+++ pybind11/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import sys
 
-if sys.version_info < (3, 8):
+if sys.version_info < (3, 8):  # noqa: UP036
     msg = "pybind11 does not support Python < 3.8. v2.13 was the last release supporting Python 3.7."
     raise ImportError(msg)
 
