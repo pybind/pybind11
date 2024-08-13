bd67643652d3800837f1f41549a2a5adbaa3fafe
Henry Schreiner <henryschreineriii@gmail.com>
2024-08-13 13:27:11 -0400

    chore: prepare for 2.13.3
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -11,11 +11,11 @@
 
 #define PYBIND11_VERSION_MAJOR 2
 #define PYBIND11_VERSION_MINOR 13
-#define PYBIND11_VERSION_PATCH 2
+#define PYBIND11_VERSION_PATCH 3
 
 // Similar to Python's convention: https://docs.python.org/3/c-api/apiabiversion.html
 // Additional convention: 0xD = dev
-#define PYBIND11_VERSION_HEX 0x020D0200
+#define PYBIND11_VERSION_HEX 0x020D0300
 
 // Define some generic pybind11 helper macros for warning management.
 //
