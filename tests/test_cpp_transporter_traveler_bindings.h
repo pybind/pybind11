2df540ea4d1708880fd1d20f57355f3caa94b397
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-08-28 16:07:33 -0700

    Move `__cpp_transporter__` lambda to `py::cpp_transporter()` regular function.

--- tests/test_cpp_transporter_traveler_bindings.h
+++ tests/test_cpp_transporter_traveler_bindings.h
@@ -7,6 +7,35 @@
 #include <cstring>
 #include <string>
 
+PYBIND11_NAMESPACE_BEGIN(pybind11)
+
+template <typename T>
+object cpp_transporter(handle self,
+                       const str &pybind11_platform_abi_id,
+                       const str &cpp_typeid_name,
+                       const str &pointer_kind) {
+    auto pointer_kind_cpp = pointer_kind.cast<std::string>();
+    if (pybind11_platform_abi_id.cast<std::string>() != PYBIND11_PLATFORM_ABI_ID) {
+        if (pointer_kind_cpp == "query_mismatch") {
+            return cast("pybind11_platform_abi_id_mismatch");
+        }
+        return none();
+    }
+    if (cpp_typeid_name.cast<std::string>() != typeid(T).name()) {
+        if (pointer_kind_cpp == "query_mismatch") {
+            return cast("cpp_typeid_name_mismatch");
+        }
+        return none();
+    }
+    if (pointer_kind_cpp != "raw_pointer_ephemeral") {
+        throw std::runtime_error("Unknown pointer_kind: \"" + pointer_kind_cpp + "\"");
+    }
+    auto *self_cpp_ptr = cast<T *>(self);
+    return capsule(static_cast<void *>(self_cpp_ptr), typeid(T).name());
+}
+
+PYBIND11_NAMESPACE_END(pybind11)
+
 namespace pybind11_tests {
 namespace test_cpp_transporter {
 
@@ -18,31 +47,7 @@ inline void wrap_traveler(py::module_ m) {
 
     py::class_<Traveler>(m, "Traveler")
         .def(py::init<std::string>())
-        .def("__cpp_transporter__",
-             [](py::handle self,
-                const py::str &pybind11_platform_abi_id,
-                const py::str &cpp_typeid_name,
-                const py::str &pointer_kind) -> py::object {
-                 auto pointer_kind_cpp = pointer_kind.cast<std::string>();
-                 if (pybind11_platform_abi_id.cast<std::string>() != PYBIND11_PLATFORM_ABI_ID) {
-                     if (pointer_kind_cpp == "query_mismatch") {
-                         return py::cast("pybind11_platform_abi_id_mismatch");
-                     }
-                     return py::none();
-                 }
-                 if (cpp_typeid_name.cast<std::string>() != typeid(Traveler).name()) {
-                     if (pointer_kind_cpp == "query_mismatch") {
-                         return py::cast("cpp_typeid_name_mismatch");
-                     }
-                     return py::none();
-                 }
-                 if (pointer_kind_cpp != "raw_pointer_ephemeral") {
-                     throw std::runtime_error("Unknown pointer_kind: \"" + pointer_kind_cpp
-                                              + "\"");
-                 }
-                 auto *self_cpp_ptr = py::cast<Traveler *>(self);
-                 return py::capsule(static_cast<void *>(self_cpp_ptr), typeid(Traveler).name());
-             })
+        .def("__cpp_transporter__", py::cpp_transporter<Traveler>)
         .def_readwrite("luggage", &Traveler::luggage);
 
     m.def("get_luggage", [](const Traveler &person) { return person.luggage; });
