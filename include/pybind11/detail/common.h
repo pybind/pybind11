95d943ae0ebdf609bbd650d119fda539509929b6
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2024-09-13 15:13:26 -0700

    Increment `PYBIND11_VERSION_PATCH`
    QA:
    
    ```
    $ nox -s tests_packaging
    nox > Running session tests_packaging
    nox > Creating virtual environment (virtualenv) using python3 in .nox/tests_packaging
    nox > python -m pip install -r tests/requirements.txt --prefer-binary
    nox > pytest tests/extra_python_package
    =========================================================== test session starts ============================================================
    platform darwin -- Python 3.10.2, pytest-7.0.0, pluggy-1.5.0
    rootdir: /Users/rgrossekunst/forked/pybind11/tests/extra_python_package, configfile: pytest.ini
    plugins: timeout-2.3.1
    collected 4 items
    
    tests/extra_python_package/test_files.py ....                                                                                        [100%]
    
    ============================================================ 4 passed in 33.98s ============================================================
    nox > Session tests_packaging was successful.
    ```

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -11,11 +11,11 @@
 
 #define PYBIND11_VERSION_MAJOR 2
 #define PYBIND11_VERSION_MINOR 11
-#define PYBIND11_VERSION_PATCH 1
+#define PYBIND11_VERSION_PATCH 2
 
 // Similar to Python's convention: https://docs.python.org/3/c-api/apiabiversion.html
 // Additional convention: 0xD = dev
-#define PYBIND11_VERSION_HEX 0x020B0100
+#define PYBIND11_VERSION_HEX 0x020B0200
 
 // Define some generic pybind11 helper macros for warning management.
 //
