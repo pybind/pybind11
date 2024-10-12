5965a65fc1205ff57731193d81d149ff60751262
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-10-12 12:02:26 +0100

    Fixed is_same_or_base_of reference

--- tests/test_class.cpp
+++ tests/test_class.cpp
@@ -62,14 +62,14 @@ class Args : public py::args {};
 } // namespace test_class
 
 static_assert(
-    !py::is_same_or_base_of<py::args,
+    !py::detail::is_same_or_base_of<py::args,
                             test_class::pr5396_forward_declared_class::ForwardClass>::value);
 static_assert(
-    !py::is_same_or_base_of<py::args,
+    !py::detail::is_same_or_base_of<py::args,
                             test_class::pr5396_forward_declared_class::ForwardClassPtr>::value);
-static_assert(py::is_same_or_base_of<py::args, py::args>::value);
+static_assert(py::detail::is_same_or_base_of<py::args, py::args>::value);
 static_assert(
-    py::is_same_or_base_of<py::args, test_class::pr5396_forward_declared_class::Args>::value);
+    py::detail::is_same_or_base_of<py::args, test_class::pr5396_forward_declared_class::Args>::value);
 
 TEST_SUBMODULE(class_, m) {
     m.def("obj_class_name", [](py::handle obj) { return py::detail::obj_class_name(obj.ptr()); });
