5b9078d6f5b8c16376a19c6a993f3fd0c9c472f0
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2024-10-13 11:27:02 -0700

    Minor cleanup of `PYBIND11_BUILD_ABI` logic/comments. Eliminate fall-through for MSVC.

--- include/pybind11/detail/internals.h
+++ include/pybind11/detail/internals.h
@@ -310,15 +310,17 @@ struct type_info {
 #    endif
 #endif
 
-/// On Linux/OSX, changes in __GXX_ABI_VERSION__ indicate ABI incompatibility.
-/// On MSVC, mixing /MT and /MD will result in crashes. See (#4953)
 #ifndef PYBIND11_BUILD_ABI
-#    if defined(__GXX_ABI_VERSION)
+#    if defined(__GXX_ABI_VERSION) // Linux/OSX.
 #        define PYBIND11_BUILD_ABI "_cxxabi" PYBIND11_TOSTRING(__GXX_ABI_VERSION)
-#    elif defined(_MSC_VER) && defined(_MT)
-#        define PYBIND11_BUILD_ABI "_mt_mscver" PYBIND11_TOSTRING(_MSC_VER)
-#    elif defined(_MSC_VER) && defined(_MD)
-#        define PYBIND11_BUILD_ABI "_md_mscver" PYBIND11_TOSTRING(((int) (_MSC_VER) / 100))
+#    elif defined(_MSC_VER) // See PR #4953.
+#        if defined(_MT)
+#            define PYBIND11_BUILD_ABI "_mt_mscver" PYBIND11_TOSTRING(_MSC_VER)
+#        elif defined(_MD)
+#            define PYBIND11_BUILD_ABI "_md_mscver" PYBIND11_TOSTRING(((int) (_MSC_VER) / 100))
+#        else
+#            error "UNEXPECTED MSVC ENVIRONMENT: PLEASE REVISE THIS CODE."
+#        endif
 #    else
 #        define PYBIND11_BUILD_ABI ""
 #    endif
