bae6ce239ef45f4409624f174cab3fb1b6a21dd9
Michael Carlstrom <rmc@carlstrom.com>
2024-12-12 11:41:07 -0500

    remove auto on StrLit case

--- include/pybind11/typing.h
+++ include/pybind11/typing.h
@@ -281,10 +281,7 @@ struct handle_type_name<typing::Never> {
 };
 
 #if defined(PYBIND11_TYPING_H_HAS_STRING_LITERAL)
-template <auto StrLit,
-          typename std::enable_if<std::is_same<decltype(StrLit), typing::StringLiteral>::value,
-                                  int>::type
-          = 0>
+template <typing::StringLiteral StrLit>
 auto constexpr const_name() {
     return const_name(StrLit.name);
 }
