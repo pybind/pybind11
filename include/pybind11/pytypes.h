988c0396ffa16f048a95dc2290877b2151b160f6
Michael Carlstrom <rmc@carlstrom.com>
2024-12-20 14:23:58 -0800

    line up comment

--- include/pybind11/pytypes.h
+++ include/pybind11/pytypes.h
@@ -116,8 +116,8 @@ public:
     /** \rst
          Similar to the above attr functions with the difference that the templated Type
          is used to set the `__annotations__` dict value to the corresponding key. Worth nothing
-     that attr_with_type_hint is implemented in cast.h
-     \endrst */
+         that attr_with_type_hint is implemented in cast.h
+    \endrst */
     template <typename T>
     obj_attr_accessor attr_with_type_hint(handle key) const;
     template <typename T>
