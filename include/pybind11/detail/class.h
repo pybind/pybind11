23f3ef071baaecd547af22803fa91c6d1b6c069d
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2025-02-22 20:18:04 +0100

    Use PYBIND11_WARNING macros

--- include/pybind11/detail/class.h
+++ include/pybind11/detail/class.h
@@ -499,12 +499,12 @@ extern "C" inline void pybind11_object_dealloc(PyObject *self) {
     Py_DECREF(type);
 }
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wredundant-decls"
+PYBIND11_WARNING_PUSH
+PYBIND11_WARNING_DISABLE_GCC("-Wredundant-decls")
 
 std::string error_string();
 
-#pragma GCC diagnostic pop
+PYBIND11_WARNING_POP
 
 /** Create the type which can be used as a common base for all classes.  This is
     needed in order to satisfy Python's requirements for multiple inheritance.
--- include/pybind11/gil.h
+++ include/pybind11/gil.h
@@ -32,13 +32,13 @@ PYBIND11_NAMESPACE_BEGIN(PYBIND11_NAMESPACE)
 
 PYBIND11_NAMESPACE_BEGIN(detail)
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wredundant-decls"
+PYBIND11_WARNING_PUSH
+PYBIND11_WARNING_DISABLE_GCC("-Wredundant-decls")
 
 // forward declarations
 PyThreadState *get_thread_state_unchecked();
 
-#pragma GCC diagnostic pop
+PYBIND11_WARNING_POP
 
 PYBIND11_NAMESPACE_END(detail)
 
