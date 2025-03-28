8757b56c5f04b604273c0c4aca280fd707787404
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-03-27 22:22:38 -0700

    Import scipy.integrate explicitly, in a way that doesn't trigger a ruff error.

--- tests/test_scipy_low_level_callable.py
+++ tests/test_scipy_low_level_callable.py
@@ -18,10 +18,13 @@ def test_get_capsule_for_scipy_LowLevelCallable():
 
 def test_with_scipy_LowLevelCallable():
     scipy = pytest.importorskip("scipy")
+    # Explicit import needed with some (older) scipy versions:
+    from scipy import integrate
+
     llc = scipy.LowLevelCallable(
         m.square.__self__.get_capsule_for_scipy_LowLevelCallable(
             signature="double (double)"
         )
     )
-    integral = scipy.integrate.quad(llc, 0, 1)
+    integral = integrate.quad(llc, 0, 1)
     assert integral[0] == pytest.approx(1 / 3, rel=1e-12)
