a952009686fd7d4bf0b41d71a740331c254a6908
Bryn Lloyd <12702862+dyollb@users.noreply.github.com>
2025-04-14 20:30:44 +0200

    adapt doc argument name

--- include/pybind11/native_enum.h
+++ include/pybind11/native_enum.h
@@ -25,9 +25,9 @@ public:
     native_enum(const object &parent_scope,
                 const char *name,
                 const char *native_type_name,
-                const char *doc = "")
+                const char *enum_doc = "")
         : detail::native_enum_data(
-              parent_scope, name, native_type_name, doc, std::type_index(typeid(EnumType))) {
+              parent_scope, name, native_type_name, enum_doc, std::type_index(typeid(EnumType))) {
         if (detail::get_local_type_info(typeid(EnumType)) != nullptr
             || detail::get_global_type_info(typeid(EnumType)) != nullptr) {
             pybind11_fail(
