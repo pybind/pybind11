34ce2d9baa59f7f0e6feab78ff282bca011d60eb
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-04-28 15:16:05 -0700

    Simplify test_class_sh_trampoline_basic.cpp,py (only one Abase is needed now)

--- tests/test_class_sh_trampoline_basic.py
+++ tests/test_class_sh_trampoline_basic.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from pybind11_tests import class_sh_trampoline_basic as m
 
 
-class PyDrvd0(m.Abase0):
+class PyDrvd(m.Abase):
     def __init__(self, val):
         super().__init__(val)
 
@@ -11,25 +11,25 @@ class PyDrvd0(m.Abase0):
         return self.Get() * 100 + other_val
 
 
-def test_drvd0_add():
-    drvd = PyDrvd0(74)
+def test_drvd_add():
+    drvd = PyDrvd(74)
     assert drvd.Add(38) == (74 * 10 + 3) * 100 + 38
 
 
-def test_drvd0_add_in_cpp_raw_ptr():
-    drvd = PyDrvd0(52)
+def test_drvd_add_in_cpp_raw_ptr():
+    drvd = PyDrvd(52)
     assert m.AddInCppRawPtr(drvd, 27) == ((52 * 10 + 3) * 100 + 27) * 10 + 7
 
 
-def test_drvd0_add_in_cpp_shared_ptr():
+def test_drvd_add_in_cpp_shared_ptr():
     while True:
-        drvd = PyDrvd0(36)
+        drvd = PyDrvd(36)
         assert m.AddInCppSharedPtr(drvd, 56) == ((36 * 10 + 3) * 100 + 56) * 100 + 11
         return  # Comment out for manual leak checking (use `top` command).
 
 
-def test_drvd0_add_in_cpp_unique_ptr():
+def test_drvd_add_in_cpp_unique_ptr():
     while True:
-        drvd = PyDrvd0(25)
+        drvd = PyDrvd(25)
         assert m.AddInCppUniquePtr(drvd, 83) == ((25 * 10 + 3) * 100 + 83) * 100 + 13
         return  # Comment out for manual leak checking (use `top` command).
