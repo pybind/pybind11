151f2255634199d73ccaddf731ec81614e05ec03
Henry Schreiner <henryschreineriii@gmail.com>
2025-05-16 18:23:29 -0400

    Revert "fix: patch for embed"
    This reverts commit c4226a06712c203d8f76debcb325e4910fb5e4ac.

--- include/pybind11/embed.h
+++ include/pybind11/embed.h
@@ -41,18 +41,15 @@
 #define PYBIND11_EMBEDDED_MODULE(name, variable)                                                  \
     static ::pybind11::module_::module_def PYBIND11_CONCAT(pybind11_module_def_, name);           \
     static void PYBIND11_CONCAT(pybind11_init_, name)(::pybind11::module_ &);                     \
-    static PyObject *PYBIND11_CONCAT(pybind11_init_wrapper_, name)() {                            \
-        static auto result = []() -> PyObject * {                                                 \
-            auto m = ::pybind11::module_::create_extension_module(                                \
-                PYBIND11_TOSTRING(name), nullptr, &PYBIND11_CONCAT(pybind11_module_def_, name));  \
-            try {                                                                                 \
-                PYBIND11_CONCAT(pybind11_init_, name)(m);                                         \
-                return m.ptr();                                                                   \
-            }                                                                                     \
-            PYBIND11_CATCH_INIT_EXCEPTIONS                                                        \
-            return nullptr;                                                                       \
-        }();                                                                                      \
-        return result;                                                                            \
+    static PyObject PYBIND11_CONCAT(*pybind11_init_wrapper_, name)() {                            \
+        auto m = ::pybind11::module_::create_extension_module(                                    \
+            PYBIND11_TOSTRING(name), nullptr, &PYBIND11_CONCAT(pybind11_module_def_, name));      \
+        try {                                                                                     \
+            PYBIND11_CONCAT(pybind11_init_, name)(m);                                             \
+            return m.ptr();                                                                       \
+        }                                                                                         \
+        PYBIND11_CATCH_INIT_EXCEPTIONS                                                            \
+        return nullptr;                                                                           \
     }                                                                                             \
     PYBIND11_EMBEDDED_MODULE_IMPL(name)                                                           \
     ::pybind11::detail::embedded_module PYBIND11_CONCAT(pybind11_module_, name)(                  \
