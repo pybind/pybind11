d9d524018c9b0ea4286cda5e487de66aba76ccaa
Tim Ohliger <ohliger.tim@gmail.com>
2025-05-23 19:04:11 +0200

    Added test to check stubgen and mypy on all tests

--- tests/test_stubgen.py
+++ tests/test_stubgen.py
@@ -39,3 +39,26 @@ def test_stubgen(tmp_path: Path) -> None:
     print(error_report)
     assert exit_status == 0
     assert "Success: no issues found in 1 source file" in normal_report
+
+
+def test_stubgen_all(tmp_path: Path) -> None:
+    # Generate stub into temporary directory
+    pybind11_stubgen.main(
+        [
+            "pybind11_tests",
+            "-o",
+            tmp_path.as_posix(),
+        ]
+    )
+    # Check stub file is generated and contains expected content
+    stub_file = tmp_path / "pybind11_tests"
+    assert stub_file.exists()
+    # stub_content = stub_file.read_text()
+    # Run mypy on the generated stub file
+    normal_report, error_report, exit_status = api.run([stub_file.as_posix()])
+    print("Normal report:")
+    print(normal_report)
+    print("Error report:")
+    print(error_report)
+    assert exit_status == 0
+    assert "Success: no issues found in 1 source file" in normal_report
