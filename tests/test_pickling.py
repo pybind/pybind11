9b96978865df793bde01fd7ed3667a410036a7d0
Henry Schreiner <henryschreineriii@gmail.com>
2025-05-26 12:34:23 -0400

    fix: prepare for 3.14 beta 2
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_pickling.py
+++ tests/test_pickling.py
@@ -69,8 +69,9 @@ def test_roundtrip(cls_name):
         pytest.param(
             "PickleableWithDict",
             marks=pytest.mark.xfail(
-                sys.version_info == (3, 14, 0, "beta", 1),
-                reason="3.14.0b1 bug: https://github.com/python/cpython/issues/133912",
+                sys.version_info == (3, 14, 0, "beta", 1)
+                or sys.version_info == (3, 14, 0, "beta", 2),
+                reason="3.14.0b1/2 bug: https://github.com/python/cpython/issues/133912",
                 strict=True,
             ),
         ),
