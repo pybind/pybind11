20f3606d4fa654f1e4a252b1d15ebf56959ba634
b-pass <b-pass@users.noreply.github.com>
2025-06-02 17:05:32 -0400

    Re-enable subinterpreters in iOS

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -256,7 +256,7 @@
 
 // Slightly faster code paths are available when PYBIND11_HAS_SUBINTERPRETER_SUPPORT is *not*
 // defined, so avoid defining it for implementations that do not support subinterpreters. However,
-// defining it unnecessarily is not expected to break anything (other than old iOS targets).
+// defining it unnecessarily is not expected to break anything.
 // This can be overridden by the user with -DPYBIND11_HAS_SUBINTERPRETER_SUPPORT=1 or 0
 #ifndef PYBIND11_HAS_SUBINTERPRETER_SUPPORT
 #    if PY_VERSION_HEX >= 0x030C0000 && !defined(PYPY_VERSION) && !defined(GRAALVM_PYTHON)
