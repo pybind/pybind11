05e756b521352a0881d459adc63bae32173ac82c
Rosdf <ab0baiih@gmail.com>
2025-06-10 21:50:52 +0400

    smaller test body

--- tests/test_const_only_smart_ptr.cpp
+++ tests/test_const_only_smart_ptr.cpp
@@ -7,103 +7,34 @@
 namespace const_only_smart_ptr {
 
 template <class T>
-class non_sync_const_shared_ptr {
+class const_only_shared_ptr {
 public:
-    non_sync_const_shared_ptr() = default;
+    const_only_shared_ptr() = default;
 
-    explicit non_sync_const_shared_ptr(const T *ptr) {
-        if (ptr) {
-            try {
-                counter_ = new size_t(1);
-            } catch (...) {
-                delete ptr;
-                throw;
-            }
-        }
+    explicit const_only_shared_ptr(const T *ptr) : ptr_(ptr) {}
 
-        ptr_ = ptr;
-    }
+    const_only_shared_ptr(const const_only_shared_ptr &) = default;
+    const_only_shared_ptr(const_only_shared_ptr &&) = default;
 
-    non_sync_const_shared_ptr(const non_sync_const_shared_ptr &other)
-        : ptr_(other.ptr_), counter_(other.counter_) {
-        if (counter_) {
-            ++*counter_;
-        }
-    }
+    const_only_shared_ptr &operator=(const const_only_shared_ptr &) = default;
+    const_only_shared_ptr &operator=(const_only_shared_ptr &&) = default;
 
-    non_sync_const_shared_ptr(non_sync_const_shared_ptr &&other) noexcept
-        : ptr_(other.ptr_), counter_(other.counter_) {
-        other.ptr_ = nullptr;
-        other.counter_ = nullptr;
-    }
+    ~const_only_shared_ptr() = default;
 
-    non_sync_const_shared_ptr &operator=(const non_sync_const_shared_ptr &other) {
-        if (this == &other) {
-            return *this;
-        }
-
-        release();
-
-        ptr_ = other.ptr_;
-        counter_ = other.counter_;
-
-        if (counter_) {
-            ++*counter_;
-        }
-
-        return *this;
-    }
-
-    non_sync_const_shared_ptr &operator=(non_sync_const_shared_ptr &&other) noexcept {
-        if (this == &other) {
-            return *this;
-        }
-
-        release();
-
-        ptr_ = other.ptr_;
-        counter_ = other.counter_;
-
-        other.ptr_ = nullptr;
-        other.counter_ = nullptr;
-
-        return *this;
-    }
-
-    ~non_sync_const_shared_ptr() { release(); }
-
-    const T *get() const { return ptr_; }
-
+    const T *get() const { return ptr_.get(); }
     const T &operator*() const { return *ptr_; }
-    const T *operator->() const { return ptr_; }
-
+    const T *operator->() const { return ptr_.get(); }
     explicit operator bool() const { return ptr_ != nullptr; }
 
 private:
-    void release() noexcept {
-        if (!counter_) {
-            return;
-        }
-
-        --*counter_;
-
-        if (*counter_ == 0) {
-            delete ptr_;
-            delete counter_;
-        }
-
-        ptr_ = nullptr;
-        counter_ = nullptr;
-    }
-
-    const T *ptr_ = nullptr;
-    size_t *counter_ = nullptr;
+    // for demonstration purpose only, this imitates smart pointer with a const-only pointer
+    std::shared_ptr<const T> ptr_;
 };
 
 class MyData {
 public:
-    static non_sync_const_shared_ptr<MyData> create(std::string name) {
-        return non_sync_const_shared_ptr<MyData>(new MyData(std::move(name)));
+    static const_only_shared_ptr<MyData> create(std::string name) {
+        return const_only_shared_ptr<MyData>(new MyData(std::move(name)));
     }
 
     const std::string &getName() const { return name_; }
@@ -117,10 +48,10 @@ private:
 
 using namespace const_only_smart_ptr;
 
-PYBIND11_DECLARE_HOLDER_TYPE(T, non_sync_const_shared_ptr<T>, true)
+PYBIND11_DECLARE_HOLDER_TYPE(T, const_only_shared_ptr<T>, true)
 
 TEST_SUBMODULE(const_module, m) {
-    py::class_<MyData, non_sync_const_shared_ptr<MyData>>(m, "Data")
+    py::class_<MyData, const_only_shared_ptr<MyData>>(m, "Data")
         .def(py::init([](const std::string &name) { return MyData::create(name); }))
         .def_property_readonly("name", &MyData::getName);
 }
