6d89ed9100bf1985db4250180f4ef0c625bb39f4
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-09-01 20:59:47 -0700

    test_animal_cat_tiger.cpp: bind_using_shared_ptr(), bind_using_smart_holder()
    Extracted from
    
    https://github.com/rwgk/pybind11/commits/animal_cat_tiger_dbgwip/
    
    https://github.com/rwgk/pybind11/commit/b187437503cda8c54c52305f3aec676f6232795b
    
    with minor modifications.

--- tests/test_animal_cat_tiger.cpp
+++ tests/test_animal_cat_tiger.cpp
@@ -5,42 +5,66 @@
 namespace pybind11_tests {
 namespace class_animal {
 
-class Animal {
-public:
-    Animal() = default;
-    Animal(const Animal &) = default;
-    Animal &operator=(const Animal &) = default;
-    virtual std::shared_ptr<Animal> clone() const = 0;
-    virtual ~Animal() = default;
+template <int> // Using int as a trick to easily generate a series of types.
+struct Multi {
+
+    class Animal {
+    public:
+        Animal() = default;
+        Animal(const Animal &) = default;
+        Animal &operator=(const Animal &) = default;
+        virtual std::shared_ptr<Animal> clone() const = 0;
+        virtual ~Animal() = default;
+    };
+
+    class Cat : virtual public Animal {
+    public:
+        Cat() = default;
+        Cat(const Cat &) = default;
+        Cat &operator=(const Cat &) = default;
+        ~Cat() override = default;
+    };
+
+    class Tiger : virtual public Cat {
+    public:
+        Tiger() = default;
+        Tiger(const Tiger &) = default;
+        Tiger &operator=(const Tiger &) = default;
+        ~Tiger() override = default;
+        std::shared_ptr<Animal> clone() const override { return std::make_shared<Tiger>(*this); }
+    };
 };
 
-class Cat : virtual public Animal {
-public:
-    Cat() = default;
-    Cat(const Cat &) = default;
-    Cat &operator=(const Cat &) = default;
-    ~Cat() override = default;
-};
+namespace py = pybind11;
 
-class Tiger : virtual public Cat {
-public:
-    Tiger() = default;
-    Tiger(const Tiger &) = default;
-    Tiger &operator=(const Tiger &) = default;
-    ~Tiger() override = default;
-    std::shared_ptr<Animal> clone() const override { return std::make_shared<Tiger>(*this); }
-};
+void bind_using_shared_ptr(py::module_ &m) {
+    using M = Multi<0>;
+
+    py::class_<M::Animal, std::shared_ptr<M::Animal>>(m, "AnimalSP");
+
+    py::class_<M::Cat, M::Animal, std::shared_ptr<M::Cat>>(m, "CatSP");
+
+    py::class_<M::Tiger, M::Cat, std::shared_ptr<M::Tiger>>(
+        m, "TigerSP", py::multiple_inheritance())
+        .def(py::init<>())
+        .def("clone", &M::Tiger::clone);
+}
+
+void bind_using_smart_holder(py::module_ &m) {
+    using M = Multi<1>;
+
+    py::class_<M::Animal, py::smart_holder>(m, "AnimalSH");
+
+    py::class_<M::Cat, M::Animal, py::smart_holder>(m, "CatSH");
+
+    py::class_<M::Tiger, M::Cat, py::smart_holder>(m, "TigerSH", py::multiple_inheritance())
+        .def(py::init<>())
+        .def("clone", &M::Tiger::clone);
+}
 
 TEST_SUBMODULE(class_animal, m) {
-    namespace py = pybind11;
-
-    py::class_<Animal, py::smart_holder>(m, "Animal");
-
-    py::class_<Cat, Animal, py::smart_holder>(m, "Cat");
-
-    py::class_<Tiger, Cat, py::smart_holder>(m, "Tiger", py::multiple_inheritance())
-        .def(py::init<>())
-        .def("clone", &Tiger::clone);
+    bind_using_shared_ptr(m);
+    bind_using_smart_holder(m);
 }
 
 } // namespace class_animal
--- tests/test_animal_cat_tiger.py
+++ tests/test_animal_cat_tiger.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+import pytest
+
 from pybind11_tests import class_animal as m
 
 
-def test_animals():
-    tiger = m.Tiger()
-    tiger.clone()
+@pytest.mark.parametrize("tiger_type", [m.TigerSP, m.TigerSH])
+def test_with_smart_holder(tiger_type):
+    tiger = tiger_type()
+    cloned = tiger.clone()
+    assert isinstance(cloned, tiger_type)
