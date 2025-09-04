7d5f21db364776f4010154169ac16b5cd43f1e1f
Scott Wolchok <swolchok@meta.com>
2025-09-04 14:10:54 -0700

    NOT FOR MERGE -- testing to see if windows-latest jobs are broken

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -9,6 +9,7 @@
 
 #pragma once
 
+// kick CI -- are we broken on windows-latest?
 #include <pybind11/conduit/wrap_include_python_h.h>
 #if PY_VERSION_HEX < 0x03080000
 #    error "PYTHON < 3.8 IS UNSUPPORTED. pybind11 v2.13 was the last to support Python 3.7."
