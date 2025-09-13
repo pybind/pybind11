8b79a85b424ff721815e4937cee29017d046b4da
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-09-12 20:57:31 -0700

    Changes suggested by at-henryiii

--- tests/test_iostream.py
+++ tests/test_iostream.py
@@ -6,15 +6,16 @@ from io import StringIO
 
 import pytest
 
+import env
 from pybind11_tests import iostream as m
 
-if sys.platform == "win32":
+if env.WIN:
     wv_build = sys.getwindowsversion().build
     skip_if_ge = 26100
     if wv_build >= skip_if_ge:
         pytest.skip(
             f"Windows build {wv_build} >= {skip_if_ge}:"
-            " Skipping iostream capture (redirection regression under investigation)",
+            " Skipping iostream capture (redirection regression needs investigation)",
             allow_module_level=True,
         )
 
