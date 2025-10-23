7948b27d4dc77cfba27f3ceafd7b350627030bd7
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-10-23 17:37:19 +0100

    Allow noconvert float to accept int

--- include/pybind11/cast.h
+++ include/pybind11/cast.h
@@ -252,8 +252,8 @@ public:
         auto index_check = [](PyObject *o) { return hasattr(o, "__index__"); };
 #endif
 
-            if (convert || PyFloat_Check(src.ptr())) {
         if constexpr (std::is_floating_point<T>::value) {
+            if (convert || PyFloat_Check(src.ptr()) || PYBIND11_LONG_CHECK(src.ptr())) {
                 py_value = (py_type) PyFloat_AsDouble(src.ptr());
             } else {
                 return false;
