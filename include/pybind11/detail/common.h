3b54dcfb618ed12d1f33cb5f2f5a5866a162776c
b-pass <b-pass@users.noreply.github.com>
2025-12-25 17:08:49 -0500

    Continue to do the ensure in both places, there might be a reason it was where it was...
    Should not hurt anything to do it extra times here.

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -445,6 +445,7 @@ PyModuleDef_Init should be treated like any other PyObject (so not shared across
     static int PYBIND11_CONCAT(pybind11_exec_, name)(PyObject *);                                 \
     PYBIND11_PLUGIN_IMPL(name) {                                                                  \
         PYBIND11_CHECK_PYTHON_VERSION                                                             \
+        pybind11::detail::ensure_internals();                                                     \
         static ::pybind11::detail::slots_array mod_def_slots = ::pybind11::detail::init_slots(    \
             &PYBIND11_CONCAT(pybind11_exec_, name), ##__VA_ARGS__);                               \
         static PyModuleDef def{/* m_base */ PyModuleDef_HEAD_INIT,                                \
