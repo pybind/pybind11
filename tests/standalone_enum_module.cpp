ab22a7d71acb6a0e443eae39938655612d3c0786
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2026-03-27 17:16:13 -0700

    Make standalone_enum_module.cpp more similar to #5976 reproducer. Also fix clang-tidy error.

--- tests/standalone_enum_module.cpp
+++ tests/standalone_enum_module.cpp
@@ -4,12 +4,11 @@
 
 namespace standalone_enum_module_ns {
 enum class SomeEnum { value1, value2 };
-}
+} // namespace standalone_enum_module_ns
 
 using namespace standalone_enum_module_ns;
 
 PYBIND11_MODULE(standalone_enum_module, m) {
-    pybind11::enum_<SomeEnum>(m, "SomeEnum")
-        .value("value1", SomeEnum::value1)
-        .value("value2", SomeEnum::value2);
+    pybind11::enum_<SomeEnum> some_enum_type(m, "SomeEnum");
+    some_enum_type.value("value1", SomeEnum::value1).value("value2", SomeEnum::value2);
 }
