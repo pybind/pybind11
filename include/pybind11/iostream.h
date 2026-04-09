9be164e4895251cf06f8df3799ff0aa30db8b200
Agisilaos Kounelis <kounelisagis@gmail.com>
2026-04-09 14:22:27 +0300

    fix: mark move constructor noexcept to satisfy clang-tidy

--- include/pybind11/iostream.h
+++ include/pybind11/iostream.h
@@ -185,7 +185,7 @@ public:
     }
 
     scoped_ostream_redirect(const scoped_ostream_redirect &) = delete;
-    scoped_ostream_redirect(scoped_ostream_redirect &&other)
+    scoped_ostream_redirect(scoped_ostream_redirect &&other) noexcept
         : old(other.old), costream(other.costream), buffer(std::move(other.buffer)) {
         other.old = nullptr;     // Disarm moved-from destructor
         costream.rdbuf(&buffer); // Re-point stream to our buffer
