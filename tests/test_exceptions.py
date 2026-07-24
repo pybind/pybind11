dfb22b0b6e63e8e6a54ac8dc005614de3d8d068c
Henry Schreiner <HenrySchreinerIII@gmail.com>
2026-07-24 19:47:22 -0400

    Update test_exceptions.py
    Co-authored-by: Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -124,8 +124,8 @@ def test_python_alreadyset_in_destructor(monkeypatch, capsys):
     assert triggered is True
 
     _, captured_stderr = capsys.readouterr()
-    # Python 3.15 colorizes this traceback when FORCE_COLOR is set.
-    captured_stderr = re.sub(r"\x1b\[[0-9;]*m", "", captured_stderr)
+    # Strip ANSI codes that Python 3.15+ adds to this traceback under FORCE_COLOR.
+    captured_stderr = re.sub(r"\x1b\[[0-?]*[ -/]*m", "", captured_stderr)
     assert captured_stderr.startswith("Exception ignored in: 'already_set demo'")
     assert captured_stderr.rstrip().endswith("KeyError: 'bar'")
 
