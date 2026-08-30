14e32ae23af529df8d82681c2d3064884b259a3c
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2026-08-30 14:42:37 -0700

    fix: bump internals version to 13
    The new detail::instance construction state has cross-DSO semantics that internals-v12 modules do not understand. Isolate the incompatible domains for v3.2.0 and document that future structural or semantic instance changes require another bump.

--- include/pybind11/detail/common.h
+++ include/pybind11/detail/common.h
@@ -18,16 +18,16 @@
 // See also: https://github.com/python/cpython/blob/HEAD/Include/patchlevel.h
 /* -- start version constants -- */
 #define PYBIND11_VERSION_MAJOR 3
-#define PYBIND11_VERSION_MINOR 1
+#define PYBIND11_VERSION_MINOR 2
 #define PYBIND11_VERSION_MICRO 0
 // ALPHA = 0xA, BETA = 0xB, GAMMA = 0xC (release candidate), FINAL = 0xF (stable release)
 // - The release level is set to "alpha" for development versions.
 //   Use 0xA0 (LEVEL=0xA, SERIAL=0) for development versions.
 // - For stable releases, set the serial to 0.
-#define PYBIND11_VERSION_RELEASE_LEVEL PY_RELEASE_LEVEL_FINAL
+#define PYBIND11_VERSION_RELEASE_LEVEL PY_RELEASE_LEVEL_ALPHA
 #define PYBIND11_VERSION_RELEASE_SERIAL 0
 // String version of (micro, release level, release serial), e.g.: 0a0, 0b1, 0rc1, 0
-#define PYBIND11_VERSION_PATCH 0
+#define PYBIND11_VERSION_PATCH 0a0
 /* -- end version constants -- */
 
 #if !defined(Py_PACK_FULL_VERSION)
@@ -632,7 +632,9 @@ struct nonsimple_values_and_holders {
     uint8_t *status;
 };
 
-/// The 'instance' type which needs to be standard layout (need to be able to use 'offsetof')
+/// The 'instance' type which needs to be standard layout (need to be able to use 'offsetof').
+/// Changes to this struct or to the semantics of its members require incrementing
+/// `PYBIND11_INTERNALS_VERSION`.
 struct instance {
     PyObject_HEAD
     /// Storage for pointers and holder; see simple_layout, below, for a description
